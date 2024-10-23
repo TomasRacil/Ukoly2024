4c71be1bb0ecd3d11fcd1d3828c74e6a2335b82f
Daniel Borovička <borovickadaniel8@gmail.com>
2024-10-23 14:11:25 +0200

    Update vypocty.py

--- Ukol_1/python/vypocty.py
+++ Ukol_1/python/vypocty.py
@@ -1,47 +1,58 @@
 
-def soucet(cisla: list[int]) -> int:
-    soucet = 0
-    for k in range(len(cisla)):
-        soucet += cisla[k]
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
 
-    return soucet
+int main()
+{
+    int num;
+    string zero;
+    int soucet = 0;
+    int nasobeni = 1;
+    int pocet = 0;
+    double prumer = 0;
+    double median = 0;
+    cout << "Zadejte čísla: ";
+    vector <int> numbers;
+    
 
-def soucin(cisla: list[int]) -> int:
-    nasobeni = 1
-    for k in range(len(cisla)):
-        nasobeni *= cisla[k]
+    while (true)
+    {
+        num = fgetc(stdin);
+        if (num == ',' || num == '\n' || num == EOF)
+        {
+            int cislo = atoi(zero.c_str());
+            soucet += cislo;
+            pocet++;
+            nasobeni *= cislo;
+            numbers.push_back(cislo);
+            zero = "";
+            if (num =='\n' || num == EOF)
+            {
+                break;
+            }
+        }
+        else
+            zero += num;
+    }
+        sort(numbers.begin(), numbers.end());
 
-    return nasobeni
+        if (pocet % 2 == 0)
+        {
+            median = ((double)numbers[pocet / 2] + numbers[pocet / 2 - 1]) / 2;
+        }
+        else
+        {
+            median = numbers[pocet / 2];
+        }
+        prumer = (double)soucet / pocet;
 
-def prumer(cisla: list[int]) -> float:
-    soucet = 1
-    pocet = len(cisla)
-    for k in range(pocet):
-        soucet += cisla[k]
-    prum = float(soucet/pocet)
-
-    return prum
-
-
-def median(cisla: list[int]) -> float:
-   delka = len(cisla)
-   cisla.sort()
-   if(delka % 2 == 0):
-       return (cisla[delka//2 - 1] + cisla[delka//2]) / 2
-   else:
-       return cisla[delka//2]
-
-
-def main():
-    """Na�te vstup od u�ivatele, zavol� funkce pro v�po�et a vyp�e v�sledky."""
-    vstup = input("Zadejte seznam ��sel odd�len�ch ��rkou: ")
-    cisla = [int(cislo) for cislo in vstup.split(",")]
-
-    print("Sou�et:", soucet(cisla))
-    print("Sou�in:", soucin(cisla))
-    print("Pr�m�rn� hodnota:", prumer(cisla))
-    print("Medi�n:", median(cisla))
-
-
-if __name__ == "__main__":
-    main()
+        cout << setprecision(3) << "  Soucet: " << soucet;
+        cout << "  Nasobek: " << nasobeni;
+        cout << "  Prumer: " << prumer;
+        cout << "  Median: " << median;
+        
+        return 0;
+}
