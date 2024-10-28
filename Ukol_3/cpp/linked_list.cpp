d099f7f7946d8c26ae597f7b9cc12505350735e0
johankak <Johanka-k@email.cz>
2024-10-28 20:23:28 +0100

    linked list hotovo

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -4,64 +4,184 @@
 struct Node
 {
     int data;
-    Node *next;
+    Node* next;
 };
 
 // Funkce pro vytvoření nového uzlu
+//Node je struktura, obsahujici data a ukazatel na dalsi uzel
+// *newNode rika, ze newNode je ukazatel na typ Node, takze uchovava adresu
+//funkce se jmenuje createNode a prijima jeden parametr data, coz je integer, ktere chceme ulozit do noveho uzlu, funkce vraci ukazatel na typ Node
 Node *createNode(int data)
 {
-    return nullptr;
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
+
 }
 
 // Funkce pro vložení uzlu na začátek seznamu
-void insertAtBeginning(Node **head, int data)
+void insertAtBeginning(Node** head, int data)
 {
+    Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+
 }
 
 // Funkce pro vložení uzlu na konec seznamu
-void insertAtEnd(Node **head, const int data)
+void insertAtEnd(Node** head, const int data)
 {
+    Node* newNode = createNode(data);
+    if (*head == nullptr) {
+        *head = newNode;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
 // Funkce pro vložení na index
-void insertAtIndex(Node **head, int data, int index)
+void insertAtIndex(Node** head, int data, int index)
 {
+    if (index == 0) {
+        insertAtBeginning(head, data);
+        return;
+    }
+    Node *newNode = createNode(data);
+    Node* temp = *head;
+    for (int i = 0; i < index - 1 && temp != nullptr; i++) {
+        temp = temp->next;
+    }
+    if (temp == nullptr) {
+        std::cout << "Index je mimo rozsah\n";
+        delete newNode;
+        return;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
 // Funkce pro smazání uzlu ze začátku seznamu
-void deleteAtBeginning(Node **head)
+void deleteAtBeginning(Node** head)
 {
+    Node* temp = (*head)->next;
+    delete* head;
+    *head = temp;
+
 }
 
 // Funkce pro smazání uzlu z konce seznamu
-void deleteAtEnd(Node **head)
+void deleteAtEnd(Node** head)
 {
+    if ((*head)->next == nullptr) {
+        deleteAtBeginning(head);
+        return;
+
+    }
+    Node* posledni;
+    Node* predposledni;
+    posledni = *head;
+    while (posledni->next != nullptr) {
+        posledni = posledni->next;
+
+    }
+    predposledni = *head;
+    while (predposledni->next->next != nullptr) {
+        predposledni = predposledni->next->next;
+
+    }
+
+    delete posledni;
+    predposledni->next = nullptr;
+
+
 }
 
 // Funkce pro smazani uzlu na indexu
-void deleteAtIndex(Node *head, int index)
+void deleteAtIndex(Node* head, int index)
 {
+    if (head == nullptr) {
+        return;
+    }
+
+    if (index == 0) {
+        deleteAtBeginning(&head);
+        return;
+
+    }
+    Node* aktualni = head;      //ukazatel, ktery zacina na prvnim uzlu (head), ten budeme mazat
+    Node* predchozi = nullptr;      //bude slouzit k uchovani predchoziho uzlu
+    int aktualnipozice = 0;
+
+    while (aktualni != nullptr && aktualnipozice < index) {
+        predchozi = aktualni;       //aktualizujeme, aby predchozi ukazoval na aktualni uzel
+        aktualni = aktualni->next;
+        aktualnipozice++;
+
+    }
+    if (aktualni != nullptr) {
+        predchozi->next = aktualni->next;   //vynechavani uzlu, ktery chceme smazat
+        delete aktualni;
+
+    }
+
 }
 
 // Funkce pro nalezeni prvniho vyskytu
-int findFirstOccurrence(Node *head, int value)
+int findFirstOccurrence(Node* head, int value)
 {
-    return -1;
+        int index = 0;
+        while (head != nullptr) {
+            if (head->data == value) {
+                return index;
+            }
+            head = head->next;
+            index++;
+        }
+
+        return -1;
+
 }
 
 // Funkce pro třídění seznamu
-void sortList(Node **head)
+void sortList(Node** head)
 {
+    if (*head == nullptr) return;
+
+    bool swapped;
+    do {
+        swapped = false;
+        Node* current = *head;
+        while (current->next != nullptr) {
+            if (current->data > current->next->data) {
+                std::swap(current->data, current->next->data);
+                swapped = true;
+            }
+            current = current->next;
+        }
+    } while (swapped);
 }
 
 // Funkce pro smazani (dealokaci) seznamu
-void deleteList(Node **head)
+void deleteList(Node** head)
 {
+    while (*head != nullptr) {
+        deleteAtBeginning(head);
+    }
 }
 
 // Operátor pro tisk dat
-std::ostream &operator<<(std::ostream &os, Node *head)
+std::ostream& operator<<(std::ostream& os, Node* head)
 {
+    Node* current = head;
+    while (current != nullptr) {
+        os << current->data << " ";
+        current = current->next;
+    }
     return os;
 }
 
@@ -69,7 +189,7 @@ std::ostream &operator<<(std::ostream &os, Node *head)
 int main()
 {
     // Vytvoření seznamu
-    Node *head = nullptr;
+    Node* head = nullptr;
     insertAtBeginning(&head, 1);
     insertAtBeginning(&head, 2);
     insertAtEnd(&head, 3);
