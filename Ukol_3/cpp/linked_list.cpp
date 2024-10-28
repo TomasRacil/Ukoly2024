b95e9d413ccd03ee55082b5e8bb17816399c55f6
OndrejHanak666 <oh2004@seznam.cz>
2024-10-28 11:38:26 +0100

    project done

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -10,58 +10,146 @@ struct Node
 // Funkce pro vytvoření nového uzlu
 Node *createNode(int data)
 {
-    return nullptr;
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
 }
 
 // Funkce pro vložení uzlu na začátek seznamu
 void insertAtBeginning(Node **head, int data)
 {
+    Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
 }
 
 // Funkce pro vložení uzlu na konec seznamu
 void insertAtEnd(Node **head, const int data)
 {
+    Node* newNode = createNode(data);
+    if (*head == nullptr)
+    {
+        *head = nullptr;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
 // Funkce pro vložení na index
 void insertAtIndex(Node **head, int data, int index)
 {
+    if (index < 0) return;
+    if (index == 0)
+    {
+        insertAtBeginning(head, data);
+        return;
+    }
+    Node* newNode = createNode(data);
+    Node* temp = *head;
+    for (int i = 0; temp != nullptr && i < index - 1; i++)
+    {
+        if (temp == nullptr) return;
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
 }
 
 // Funkce pro smazání uzlu ze začátku seznamu
 void deleteAtBeginning(Node **head)
 {
+    if (*head == nullptr) return;
+    Node* temp = *head;
+    *head = (*head)->next;
+    delete temp;
 }
 
 // Funkce pro smazání uzlu z konce seznamu
 void deleteAtEnd(Node **head)
 {
+    if (*head == nullptr) return;
+    if ((*head)->next == nullptr)
+    {
+        delete* head;
+        *head = nullptr;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
 }
 
 // Funkce pro smazani uzlu na indexu
 void deleteAtIndex(Node *head, int index)
 {
+    if (*head == nullptr || index < 0) return;
+    if (index == 0) {
+        deleteAtBeginning(head);
+        return;
+    }
+    Node* temp = *head;
+    for (int i = 0; temp->next != nullptr && i < index - 1; i++)
+        temp = temp->next;
+
+    if (temp->next == nullptr) return;
+    Node* nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    delete nodeToDelete;
 }
 
 // Funkce pro nalezeni prvniho vyskytu
 int findFirstOccurrence(Node *head, int value)
 {
-    return -1;
+    int index = 0;
+    while (head != nullptr)
+    {
+        if (head->data == value) return index;
+        head = head->next;
+        index++;
+    }
+    return -1;          // nenalezeno
 }
 
 // Funkce pro třídění seznamu
 void sortList(Node **head)
 {
+    if (*head == nullptr || (*head)->next == nullptr) return;
+    for (Node* i = *head; i->next != nullptr; i = i->next)
+    {
+        for (Node* j = i->next; j != nullptr; j = j->next)
+        {
+            if (i->data > j->data)
+                std::swap(i->data, j->data);
+        }
+    }
 }
 
 // Funkce pro smazani (dealokaci) seznamu
 void deleteList(Node **head)
 {
+    while (*head != nullptr)
+        deleteAtBeginning(head);
 }
 
 // Operátor pro tisk dat
 std::ostream &operator<<(std::ostream &os, Node *head)
 {
+    const Node* temp = head;
+    while (temp != nullptr)
+    {
+        os << temp->data << " -> ";
+        temp = temp->next;
+    }
+    os << "nullptr";
     return os;
 }
 
