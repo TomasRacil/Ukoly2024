c412fc2dd962fd8baed1a627dcaca94422694f02
Daniel Borovička <borovickadaniel8@gmail.com>
2024-10-28 20:30:28 +0100

    Update linked_list.cpp

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -10,53 +10,179 @@ struct Node
 // Funkce pro vytvoření nového uzlu
 Node *createNode(int data)
 {
+    Node *newNode = new Node();
+    newNode -> data = data;
+    newNode -> next = nullptr;
     return nullptr;
 }
 
 // Funkce pro vložení uzlu na začátek seznamu
 void insertAtBeginning(Node **head, int data)
-{
+{ 
+    {
+    Node *newnode = new Node(data);
+    newnode->next = *head;
+    *head = newnode;
+    }
 }
 
 // Funkce pro vložení uzlu na konec seznamu
 void insertAtEnd(Node **head, const int data)
 {
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    if (*head == nullptr)
+    {
+        *head = newNode;
+        return;
+    }
+
+    Node *temp = *head;
+    while (temp->next != nullptr) 
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
 // Funkce pro vložení na index
 void insertAtIndex(Node **head, int data, int index)
 {
+    if (index == 0) 
+    {
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    Node *newNode = new Node();
+    newNode->data = data;
+
+    Node *temp = *head;
+    for (int i = 0; temp != nullptr && i < index - 1; ++i) 
+    {
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) 
+    {
+        std::cerr << "Index out of bounds" << std::endl;
+        delete newNode;
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
 // Funkce pro smazání uzlu ze začátku seznamu
 void deleteAtBeginning(Node **head)
 {
+    if (*head == nullptr) return;
+
+    Node *temp = *head;
+    *head = (*head)->next;
+    delete temp;
 }
 
 // Funkce pro smazání uzlu z konce seznamu
 void deleteAtEnd(Node **head)
 {
+    if (*head == nullptr) return;
+
+    if ((*head)->next == nullptr) 
+    {
+        delete *head;
+        *head = nullptr;
+        return;
+    }
+
+    Node *temp = *head;
+    while (temp->next->next != nullptr) 
+    {
+        temp = temp->next;
+    }
+
+    delete temp->next;
+    temp->next = nullptr;
 }
 
 // Funkce pro smazani uzlu na indexu
-void deleteAtIndex(Node *head, int index)
+void deleteAtIndex(Node **head, int index)
 {
+    if (*head == nullptr) return;
+
+    Node *temp = *head;
+
+    if (index == 0) 
+    {
+        *head = temp->next;
+        delete temp;
+        return;
+    }
+
+    for (int i = 0; temp != nullptr && i < index - 1; ++i) 
+    {
+        temp = temp->next;
+    }
+
+    if (temp == nullptr || temp->next == nullptr) 
+    {
+        std::cerr << "Index out of bounds" << std::endl;
+        return;
+    }
+
+    Node *nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    delete nodeToDelete;
 }
 
 // Funkce pro nalezeni prvniho vyskytu
 int findFirstOccurrence(Node *head, int value)
 {
+    int index = 0;
+    while (head != nullptr) 
+    {
+        if (head->data == value) 
+        {
+            return index;
+        }
+        head = head->next;
+        index++;
+    }
     return -1;
 }
 
 // Funkce pro třídění seznamu
 void sortList(Node **head)
 {
+    if (*head == nullptr || (*head)->next == nullptr) return;
+
+    Node *i = *head;
+    while (i != nullptr) 
+    {
+        Node *j = i->next;
+        while (j != nullptr) 
+        {
+            if (i->data > j->data) {
+                std::swap(i->data, j->data);
+            }
+            j = j->next;
+        }
+        i = i->next;
+    }
 }
 
 // Funkce pro smazani (dealokaci) seznamu
 void deleteList(Node **head)
 {
+    while (*head != nullptr) 
+    {
+        Node *temp = *head;
+        *head = (*head)->next;
+        delete temp;
+    }
 }
 
 // Operátor pro tisk dat
