6cbd26cf257ddc04b0d4ed31cc0cbb559ce0d3d9
ICED-B <ozzak.tomas.hronek@gmail.com>
2024-10-28 21:07:58 +0100

    NodesBoostedXD

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -10,58 +10,191 @@ struct Node
 // Funkce pro vytvoření nového uzlu
 Node *createNode(int data)
 {
-    return nullptr;
+    Node *newNode = new Node;
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
 }
 
 // Funkce pro vložení uzlu na začátek seznamu
 void insertAtBeginning(Node **head, int data)
 {
+    Node *newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
 }
 
 // Funkce pro vložení uzlu na konec seznamu
 void insertAtEnd(Node **head, const int data)
 {
+    Node *newNode = createNode(data);
+    if (*head == nullptr)
+    {
+        *head = newNode;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
 // Funkce pro vložení na index
 void insertAtIndex(Node **head, int data, int index)
 {
+    if (index == 0)
+    {
+        insertAtBeginning(head, data);
+        return;
+    }
+    Node *newNode = createNode(data);
+    Node *temp = *head;
+    for (int i = 0; i < index - 1 && temp != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+    if (temp == nullptr)
+    {
+        std::cout << "Index mimo rozsah" << std::endl;
+        return;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
 // Funkce pro smazání uzlu ze začátku seznamu
 void deleteAtBeginning(Node **head)
 {
+    if (*head == nullptr)
+    {
+        return;
+    }
+    Node *temp = *head;
+    *head = (*head)->next;
+    delete temp;
 }
 
 // Funkce pro smazání uzlu z konce seznamu
 void deleteAtEnd(Node **head)
 {
+    if (*head == nullptr)
+    {
+        return;
+    }
+    if ((*head)->next == nullptr)
+    {
+        delete *head;
+        *head = nullptr;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
 }
 
 // Funkce pro smazani uzlu na indexu
 void deleteAtIndex(Node *head, int index)
 {
+    if (head == nullptr)
+    {
+        return;
+    }
+    if (index == 0)
+    {
+        deleteAtBeginning(&head);
+        return;
+    }
+    Node *temp = head;
+    for (int i = 0; i < index - 1 && temp != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+    if (temp == nullptr || temp->next == nullptr)
+    {
+        std::cout << "Index mimo rozsah" << std::endl;
+        return;
+    }
+    Node *nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    delete nodeToDelete;
 }
 
 // Funkce pro nalezeni prvniho vyskytu
 int findFirstOccurrence(Node *head, int value)
 {
-    return -1;
+    int index = 0;
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->data == value)
+        {
+            return index;
+        }
+        temp = temp->next;
+        index++;
+    }
+    return -1; // Nenalezeno
 }
 
-// Funkce pro třídění seznamu
+// Funkce pro třídění seznamu (Bubble Sort)
 void sortList(Node **head)
 {
+    if (*head == nullptr || (*head)->next == nullptr)
+    {
+        return;
+    }
+    bool swapped;
+    Node *ptr1;
+    Node *lptr = nullptr;
+
+    do
+    {
+        swapped = false;
+        ptr1 = *head;
+
+        while (ptr1->next != lptr)
+        {
+            if (ptr1->data > ptr1->next->data)
+            {
+                std::swap(ptr1->data, ptr1->next->data);
+                swapped = true;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
 }
 
 // Funkce pro smazani (dealokaci) seznamu
 void deleteList(Node **head)
 {
+    Node *current = *head;
+    Node *next = nullptr;
+    while (current != nullptr)
+    {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+    *head = nullptr;
 }
 
 // Operátor pro tisk dat
 std::ostream &operator<<(std::ostream &os, Node *head)
 {
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        os << temp->data << " -> ";
+        temp = temp->next;
+    }
+    os << "nullptr";
     return os;
 }
 
