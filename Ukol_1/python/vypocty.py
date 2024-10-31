0f07eaa68d18aa93e5cc7d6ce0fa074685b852d9
DanielBi1ek <145032686+DanielBi1ek@users.noreply.github.com>
2024-10-31 17:49:37 +0100

    raise ValueError fix

--- Ukol_1/python/vypocty.py
+++ Ukol_1/python/vypocty.py
@@ -10,6 +10,8 @@ def soucet(cisla: list[int]) -> int:
     Returns:
       Součet čísel.
     """
+    if not cisla:
+        raise ValueError("Seznam čísel je prázdný.")
     return sum(cisla)
 
 
@@ -23,6 +25,8 @@ def soucin(cisla: list[int]) -> int:
     Returns:
       Součin čísel.
     """
+    if not cisla:
+        raise ValueError("Seznam čísel je prázdný.")
     return math.prod(cisla)
 
 
@@ -35,6 +39,9 @@ def prumer(cisla: list[int]) -> float:
     Returns:
       Průměrná hodnota čísel.
     """
+    if not cisla:
+        raise ValueError("Seznam čísel je prázdný.")
+
     return sum(cisla) / len(cisla)
 
 
@@ -48,6 +55,8 @@ def median(cisla: list[int]) -> float:
       Medián čísel.
     """
     cisla.sort()
+    if not cisla:
+        raise ValueError("Seznam čísel je prázdný.")  # Teoreticky by tu stačil jen tento raie VError, ale pokud chápu testy správně tak si volají funkci jako takovou proto je toto v každé funkci
     delka = len(cisla)
     if delka % 2 == 0:
         return (cisla[delka // 2 - 1] + cisla[delka // 2]) / 2
@@ -60,10 +69,8 @@ def main():
     vstup = input("Zadejte seznam čísel oddělených čárkou: ")
     cisla = [int(cislo) for cislo in vstup.split(",") if cislo.strip() != "" and cislo.strip().isdigit()]
     if not cisla:
-        print("Nebyla zadána žádná čísla.")
-        return
-
-
+        raise ValueError("Nebyla zadána žádná čísla.")  #
+        
     print("Součet:", soucet(cisla))
     print("Součin:", soucin(cisla))
     print("Průměrná hodnota:", prumer(cisla))
