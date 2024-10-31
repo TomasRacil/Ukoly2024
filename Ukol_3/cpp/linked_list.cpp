b9a753ef5fc94d7828310d3ce44a05bdec53e198
terezavasickova <terka.vasickova03@gmail.com>
2024-10-31 20:59:50 +0100

    Update linked_list.cpp

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -10,62 +10,166 @@ struct Node
 // Funkce pro vytvoření nového uzlu
 Node *createNode(int data)
 {
-    return nullptr;
+    Node *newNode = new Node;
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
 }
 
 // Funkce pro vložení uzlu na začátek seznamu
 void insertAtBeginning(Node **head, int data)
 {
+    Node *newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
 }
 
 // Funkce pro vložení uzlu na konec seznamu
 void insertAtEnd(Node **head, const int data)
 {
+    Node *newNode = createNode(data);
+    if (*head == nullptr)
+    {
+        *head = newNode;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
-// Funkce pro vložení na index
+// Funkce pro vložení uzlu na specifický index
 void insertAtIndex(Node **head, int data, int index)
 {
+    if (index == 0)
+    {
+        insertAtBeginning(head, data);
+        return;
+    }
+    Node *newNode = createNode(data);
+    Node *temp = *head;
+    for (int i = 0; i < index - 1 && temp != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+    if (temp == nullptr)
+    {
+        delete newNode;
+        return; // Index je mimo rozsah
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
 // Funkce pro smazání uzlu ze začátku seznamu
 void deleteAtBeginning(Node **head)
 {
+    if (*head == nullptr)
+        return;
+    Node *temp = *head;
+    *head = (*head)->next;
+    delete temp;
 }
 
 // Funkce pro smazání uzlu z konce seznamu
 void deleteAtEnd(Node **head)
 {
+    if (*head == nullptr)
+        return;
+    if ((*head)->next == nullptr)
+    {
+        delete *head;
+        *head = nullptr;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
 }
 
-// Funkce pro smazani uzlu na indexu
+// Funkce pro smazání uzlu na specifickém indexu
 void deleteAtIndex(Node *head, int index)
 {
+    if (index == 0)
+    {
+        deleteAtBeginning(&head);
+        return;
+    }
+    Node *temp = head;
+    for (int i = 0; i < index - 1 && temp != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+    if (temp == nullptr || temp->next == nullptr)
+        return; // Index je mimo rozsah
+    Node *toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete toDelete;
 }
 
-// Funkce pro nalezeni prvniho vyskytu
+// Funkce pro nalezení prvního výskytu dané hodnoty
 int findFirstOccurrence(Node *head, int value)
 {
-    return -1;
+    int index = 0;
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->data == value)
+            return index;
+        temp = temp->next;
+        index++;
+    }
+    return -1; // Hodnota nebyla nalezena
 }
 
 // Funkce pro třídění seznamu
 void sortList(Node **head)
 {
+    if (*head == nullptr || (*head)->next == nullptr)
+        return; // Pokud je seznam prázdný nebo má jen jeden uzel
+
+    for (Node *i = *head; i->next != nullptr; i = i->next)
+    {
+        for (Node *j = *head; j->next != nullptr; j = j->next)
+        {
+            if (j->data > j->next->data)
+            {
+                std::swap(j->data, j->next->data);
+            }
+        }
+    }
 }
 
-// Funkce pro smazani (dealokaci) seznamu
+// Funkce pro smazání celého seznamu
 void deleteList(Node **head)
 {
+    while (*head != nullptr)
+    {
+        deleteAtBeginning(head);
+    }
 }
 
-// Operátor pro tisk dat
+// Přetížení operátoru << pro tisk seznamu
 std::ostream &operator<<(std::ostream &os, Node *head)
 {
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        os << temp->data << " -> ";
+        temp = temp->next;
+    }
+    os << "nullptr";
     return os;
 }
 
-#ifndef __TEST__ // Add this preprocessor guard
+#ifndef __TEST__ // Preprocessor guard
 int main()
 {
     // Vytvoření seznamu
