a48ae1480250b360e417fc528c43550a278c4ece
Daniel Borovička <borovickadaniel8@gmail.com>
2024-11-01 12:05:00 +0100

    Update vypocty.py

--- Ukol_1/python/vypocty.py
+++ Ukol_1/python/vypocty.py
@@ -1,46 +1,47 @@
-
 def soucet(cisla: list[int]) -> int:
-    soucet = 0
-    for k in range(len(cisla)):
-        soucet += cisla[k]
-
-    return soucet
+  soucet = 0
+  for k in range(len(cisla)):
+    soucet += cisla[i]
+  
+  return soucet
 
 def soucin(cisla: list[int]) -> int:
-    nasobeni = 1
-    for k in range(len(cisla)):
-        nasobeni *= cisla[k]
+  soucin = 1
+  for k in range(len(cisla)):
+    soucin *= cisla[i]
+  
+  return soucin
 
-    return nasobeni
 
 def prumer(cisla: list[int]) -> float:
-    soucet = 1
-    pocet = len(cisla)
-    for k in range(pocet):
-        soucet += cisla[k]
-    prum = float(soucet/pocet)
-
-    return prum
+  soucet = 0
+  pocet = 0
+  for k in range(len(cisla)):
+    soucet += cisla[i]
+    pocet += 1
+  prum = float(soucet/pocet)
+  
+  return prum
 
 
 def median(cisla: list[int]) -> float:
-   delka = len(cisla)
-   cisla.sort()
-   if(delka % 2 == 0):
-       return (cisla[delka//2 - 1] + cisla[delka//2]) / 2
-   else:
-       return cisla[delka//2]
+  delka = len(cisla)
+  cisla.sort()
+  if(delka % 2 == 0):
+    return (cisla[delka//2 - 1] + cisla[delka//2]) / 2
+  else:
+    return cisla[delka//2]
 
 
 def main():
-    """Na�te vstup od u�ivatele, zavol� funkce pro v�po�et a vyp�e v�sledky."""
-    vstup = input("Zadejte seznam ��sel odd�len�ch ��rkou: ")
+    """Načte vstup od uživatele, zavolá funkce pro výpočet a vypíše výsledky."""
+    vstup = input("Zadejte seznam čísel oddělených čárkou: ")
     cisla = [int(cislo) for cislo in vstup.split(",")]
 
-    print("Sou�et:", soucet(cisla))
-    print("Sou�in:", soucin(cisla))
-    print("Pr�m�rn� hodnota:", prumer(cisla))
-    print("Medi�n:", median(cisla))
+    print("Součet:", soucet(cisla))
+    print("Součin:", soucin(cisla))
+    print("Průměrná hodnota:", prumer(cisla))
+    print("Medián:", median(cisla))
 
 
 if __name__ == "__main__":
