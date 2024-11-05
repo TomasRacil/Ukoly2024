804364e275d6e86c7ece2bac1fe91fec47b528dc
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2024-11-05 21:30:01 +0100

    modified:   Ukol_4/cpp/advent_lod.cpp

--- Ukol_4/cpp/advent_lod.cpp
+++ Ukol_4/cpp/advent_lod.cpp
@@ -1,16 +1,92 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+using namespace std;
 
 class Lod
 {
 private:
+    int x,y;
+    int direction = 0;
+    //char direction = 'E'; //W,E,N,S
 public:
     Lod(int x, int y, char smer, int cilovy_bod_x, int cilovy_bod_y)
     {
+        this->x = x;
+        this->y = y;
+        switch (smer){
+            case 'E':
+                direction = 0;
+                break;
+            case 'W':
+                direction = 180;
+                break;
+            case 'N':
+                direction = 90;
+                break;
+            case 'S':
+                direction = 270;
+        }
     }
     int naviguj(std::string cesta_soubor, bool druhe_reseni)
     {
-        return 0;
+        //try to open the file
+        ifstream file (cesta_soubor, ifstream::in);
+        if (!file.is_open()) {
+            //file is not open
+            cerr << "Error opening file\n";
+            return -1;
+        if (file.bad()) {
+            cerr << "Fatal error: badbit is set!\n";
+            return -1;
+        }
+
+         string instruction;
+         int negr = stoi(instruction.substr(1));
+
+         while(42) {
+            getline(file, instruction);
+            if (file.eof())
+                break;
+            
+            switch(instruction[0]) {
+                case 'N':
+                    y += negr;
+                    break;
+                case 'S':
+                    y -= negr;
+                    break;
+                case 'E':
+                    x += negr;
+                    break;
+                case 'W':
+                    x -= negr;
+                    break;
+                case 'L':
+                    direction = (direction + negr) % 360;
+                    break;
+                case 'R':
+                    direction = (direction - negr) % 360;
+                    break;
+                case 'F':
+                    switch(direction) {
+                        case 90:
+                            y += negr;
+                            break;
+                        case 270:
+                            y -= negr;
+                            break;
+                        case 0:
+                            x += negr;
+                            break;
+                        case 180:
+                            x -= negr;
+                    }
+                    break;
+            }
+         }
+  }
+
     }
 };
 
