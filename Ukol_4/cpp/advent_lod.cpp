f4ff6746d3e6a6111f488cf926fc3804baba9972
NikACR <holynikodem1@seznam.cz>
2024-11-07 07:26:11 +0000

    F change

--- Ukol_4/cpp/advent_lod.cpp
+++ Ukol_4/cpp/advent_lod.cpp
@@ -1,110 +1,102 @@
-// advent_lod.cpp
-
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
 #include <cmath>
+#include <string>
 
-using namespace std;
-
-class Lod {
+class Lod
+{
 private:
-    int x;       // Souřadnice lodi X
-    int y;       // Souřadnice lodi Y
-    char smer;   // Směr lodi ('N', 'S', 'E', 'W')
-    int wx;      // Waypoint X
-    int wy;      // Waypoint Y
+    int x, y;            // Souřadnice lodi
+    int uhel;            // Směr lodi ve stupních (0 = východ, 90 = jih, 180 = západ, 270 = sever)
+    int wx, wy;          // Waypoint souřadnice (relativní k lodi)
 
 public:
-    // Konstruktor
-    Lod(int xStart, int yStart, char pocatecniSmer, int wxStart, int wyStart) {
-        x = xStart;
-        y = yStart;
-        smer = pocatecniSmer;
-        wx = wxStart;
-        wy = wyStart;
+    Lod(int x_start, int y_start, char smer, int wx_start, int wy_start)
+        : x(x_start), y(y_start), wx(wx_start), wy(wy_start)
+    {
+        // Nastavení úhlu podle počátečního směru
+        switch (smer) {
+            case 'N': uhel = 270; break;
+            case 'E': uhel = 0; break;
+            case 'S': uhel = 90; break;
+            case 'W': uhel = 180; break;
+            default: uhel = 0;
+        }
     }
 
-    // Metoda naviguj
-    int naviguj(const string& nazevSouboru, bool pouzitWaypoint) {
-        ifstream soubor(nazevSouboru);
-        string instrukce;
-        vector<string> instrukceSeznam;
+    int naviguj(const std::string& cesta_soubor, bool druhe_reseni)
+    {
+        std::ifstream soubor(cesta_soubor);
+        if (!soubor.is_open()) {
+            std::cerr << "Nepodařilo se otevřít soubor: " << cesta_soubor << std::endl;
+            return -1;
+        }
 
+        std::string instrukce;
         while (getline(soubor, instrukce)) {
-            instrukceSeznam.push_back(instrukce);
-        }
+            char akce = instrukce[0];
+            int hodnota = std::stoi(instrukce.substr(1));
 
-        if (!pouzitWaypoint) {
-            int uhel = 0; // 0 stupňů = východ
-
-            for (const string& instr : instrukceSeznam) {
-                char akce = instr[0];
-                int hodnota = stoi(instr.substr(1));
-
-                if (akce == 'N') y += hodnota;
-                else if (akce == 'S') y -= hodnota;
-                else if (akce == 'E') x += hodnota;
-                else if (akce == 'W') x -= hodnota;
-
-                else if (akce == 'L') {
-                    uhel = (uhel - hodnota + 360) % 360;
-                }
-                else if (akce == 'R') {
-                    uhel = (uhel + hodnota) % 360;
-                }
-                else if (akce == 'F') {
-                    if (uhel == 0) x += hodnota;
-                    else if (uhel == 90) y -= hodnota;
-                    else if (uhel == 180) x -= hodnota;
-                    else if (uhel == 270) y += hodnota;
-                    else {
-                        // Pro úhly, které nejsou násobkem 90°
-                        double rad = uhel * M_PI / 180.0;
-                        x += round(cos(rad) * hodnota);
-                        y -= round(sin(rad) * hodnota);
+            if (druhe_reseni) {
+                // Řešení s waypointem
+                switch (akce) {
+                    case 'N': wy += hodnota; break;
+                    case 'S': wy -= hodnota; break;
+                    case 'E': wx += hodnota; break;
+                    case 'W': wx -= hodnota; break;
+                    case 'L':
+                    case 'R': {
+                        int rotace = (akce == 'L') ? hodnota : 360 - hodnota;
+                        rotace = (rotace + 360) % 360;
+                        int puvodni_wx = wx, puvodni_wy = wy;
+                        if (rotace == 90) {
+                            wx = -puvodni_wy;
+                            wy = puvodni_wx;
+                        } else if (rotace == 180) {
+                            wx = -puvodni_wx;
+                            wy = -puvodni_wy;
+                        } else if (rotace == 270) {
+                            wx = puvodni_wy;
+                            wy = -puvodni_wx;
+                        }
+                        break;
+                    }
+                    case 'F': {
+                        x += wx * hodnota;
+                        y += wy * hodnota;
+                        break;
                     }
                 }
-            }
-        }
-        else {
-            for (const string& instr : instrukceSeznam) {
-                char akce = instr[0];
-                int hodnota = stoi(instr.substr(1));
-
-                if (akce == 'N') wy += hodnota;
-                else if (akce == 'S') wy -= hodnota;
-                else if (akce == 'E') wx += hodnota;
-                else if (akce == 'W') wx -= hodnota;
-
-                else if (akce == 'L' || akce == 'R') {
-                    int uhel = hodnota;
-                    if (akce == 'R') uhel = -hodnota;
-                    uhel = (uhel + 360) % 360;
-
-                    int puvodniWx = wx;
-                    int puvodniWy = wy;
-
-                    if (uhel == 90) {
-                        wx = -puvodniWy;
-                        wy = puvodniWx;
-                    } else if (uhel == 180) {
-                        wx = -puvodniWx;
-                        wy = -puvodniWy;
-                    } else if (uhel == 270) {
-                        wx = puvodniWy;
-                        wy = -puvodniWx;
-                    }
-                }
-
-                else if (akce == 'F') {
-                    x += wx * hodnota;
-                    y += wy * hodnota;
+            } else {
+                // Řešení bez waypointu
+                switch (akce) {
+                    case 'N': y += hodnota; break;
+                    case 'S': y -= hodnota; break;
+                    case 'E': x += hodnota; break;
+                    case 'W': x -= hodnota; break;
+                    case 'L': uhel = (uhel - hodnota + 360) % 360; break;
+                    case 'R': uhel = (uhel + hodnota) % 360; break;
+                    case 'F':
+                        if (uhel == 0) x += hodnota;
+                        else if (uhel == 90) y -= hodnota;
+                        else if (uhel == 180) x -= hodnota;
+                        else if (uhel == 270) y += hodnota;
+                        break;
                 }
             }
         }
 
-        return abs(x) + abs(y);
+        return std::abs(x) + std::abs(y); // Výpočet Manhattan vzdálenosti
     }
 };
+
+#ifndef __TEST__
+int main()
+{
+    Lod lod(0, 0, 'E', 10, 1);
+    std::cout << "Manhattan vzdálenost (bez waypointu): " << lod.naviguj("vstup_1.txt", false) << std::endl;
+    Lod lod2(0, 0, 'E', 10, 1);
+    std::cout << "Manhattan vzdálenost (s waypointem): " << lod2.naviguj("vstup_1.txt", true) << std::endl;
+    return 0;
+}
+#endif // __TEST__
