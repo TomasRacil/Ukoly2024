50473f30a937e68ce4f8c17f8d15d505d4af47f6
JiriBarta <jirib3@gmail.com>
2024-11-11 18:16:30 +0100

    Update vypocty.py

--- Ukol_1/python/vypocty.py
+++ Ukol_1/python/vypocty.py
@@ -1,60 +1,40 @@
 def soucet(cisla: list[int]) -> int:
-    """Vypočítá součet čísel v seznamu.
 
-    Args:
-      cisla: Seznam čísel.
-
-    Returns:
-      Součet čísel.
-    """
-    pass
+    return sum(cisla)
 
 
 def soucin(cisla: list[int]) -> int:
-    """Vypočítá součin čísel v seznamu.
 
-    Args:
-      cisla: Seznam čísel.
-
-    Returns:
-      Součin čísel.
-    """
-    pass  # TODO: Implementujte funkci
+    soucin = 1
+    for cislo in cisla:
+        soucin *= cislo
+    return soucin
 
 
 def prumer(cisla: list[int]) -> float:
-    """Vypočítá průměrnou hodnotu čísel v seznamu.
 
-    Args:
-      cisla: Seznam čísel.
-
-    Returns:
-      Průměrná hodnota čísel.
-    """
-    pass
+    return sum(cisla) / len(cisla)
 
 
 def median(cisla: list[int]) -> float:
-    """Vypočítá medián čísel v seznamu.
 
-    Args:
-      cisla: Seznam čísel.
-
-    Returns:
-      Medián čísel.
-    """
-    pass  # TODO: Implementujte funkci
+    cisla = sorted(cisla)
+    n = len(cisla)
+    if n % 2 == 1:
+        return cisla[n // 2]
+    else:
+        return (cisla[n // 2 - 1] + cisla[n // 2]) / 2.0
 
 
 def main():
-    """Načte vstup od uživatele, zavolá funkce pro výpočet a vypíše výsledky."""
-    vstup = input("Zadejte seznam čísel oddělených čárkou: ")
+
+    vstup = input("Zadejte seznam cisel oddelenych carkou: ")
     cisla = [int(cislo) for cislo in vstup.split(",")]
 
-    print("Součet:", soucet(cisla))
-    print("Součin:", soucin(cisla))
-    print("Průměrná hodnota:", prumer(cisla))
-    print("Medián:", median(cisla))
+    print("Soucet:", soucet(cisla))
+    print("Soucin:", soucin(cisla))
+    print("Prumerna hodnota:", prumer(cisla))
+    print("Median:", median(cisla))
 
 
 if __name__ == "__main__":
