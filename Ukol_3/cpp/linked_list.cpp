f377e4700041d0d4c5356f612cc29fc104e9a4c9
DominikHavelunob <dominik.havel@unob.cz>
2024-11-13 14:27:50 +0100

    linked_list.cpp

--- Ukol_3/cpp/linked_list.cpp
+++ Ukol_3/cpp/linked_list.cpp
@@ -10,58 +10,173 @@ struct Node
 // Funkce pro vytvoření nového uzlu
 Node *createNode(int data)
 {
-    return nullptr;
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
 }
 
 // Funkce pro vložení uzlu na začátek seznamu
 void insertAtBeginning(Node **head, int data)
 {
+    Node *newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
 }
 
 // Funkce pro vložení uzlu na konec seznamu
 void insertAtEnd(Node **head, const int data)
 {
+    Node *newNode = createNode(data);
+    if (*head == nullptr)
+    {
+        *head = newNode;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
 }
 
-// Funkce pro vložení na index
+// Funkce pro vložení uzlu na specifický index
 void insertAtIndex(Node **head, int data, int index)
 {
+    if (index == 0)
+    {
+        insertAtBeginning(head, data);
+        return;
+    }
+    Node *newNode = createNode(data);
+    Node *temp = *head;
+    for (int i = 0; i < index - 1 && temp != nullptr; ++i)
+    {
+        temp = temp->next;
+    }
+    if (temp == nullptr)
+    {
+        std::cerr << "Index is out of bounds" << std::endl;
+        delete newNode;
+        return;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
 // Funkce pro smazání uzlu ze začátku seznamu
 void deleteAtBeginning(Node **head)
 {
+    if (*head == nullptr) return;
+    Node *temp = *head;
+    *head = (*head)->next;
+    delete temp;
 }
 
 // Funkce pro smazání uzlu z konce seznamu
 void deleteAtEnd(Node **head)
 {
+    if (*head == nullptr) return;
+    if ((*head)->next == nullptr)
+    {
+        delete *head;
+        *head = nullptr;
+        return;
+    }
+    Node *temp = *head;
+    while (temp->next->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
 }
 
-// Funkce pro smazani uzlu na indexu
+// Funkce pro smazání uzlu na specifickém indexu
 void deleteAtIndex(Node *head, int index)
 {
+    if (head == nullptr) return;
+    if (index == 0)
+    {
+        std::cerr << "Cannot delete head node with deleteAtIndex" << std::endl;
+        return;
+    }
+    Node *temp = head;
+    for (int i = 0; i < index - 1 && temp != nullptr; ++i)
+    {
+        temp = temp->next;
+    }
+    if (temp == nullptr || temp->next == nullptr)
+    {
+        std::cerr << "Index is out of bounds" << std::endl;
+        return;
+    }
+    Node *nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    delete nodeToDelete;
 }
 
-// Funkce pro nalezeni prvniho vyskytu
+// Funkce pro nalezení prvního výskytu hodnoty
 int findFirstOccurrence(Node *head, int value)
 {
+    int index = 0;
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->data == value)
+        {
+            return index;
+        }
+        temp = temp->next;
+        ++index;
+    }
     return -1;
 }
 
-// Funkce pro třídění seznamu
+// Funkce pro třídění seznamu (Bubble Sort)
 void sortList(Node **head)
 {
+    if (*head == nullptr) return;
+    bool swapped;
+    do
+    {
+        swapped = false;
+        Node *temp = *head;
+        while (temp->next != nullptr)
+        {
+            if (temp->data > temp->next->data)
+            {
+                std::swap(temp->data, temp->next->data);
+                swapped = true;
+            }
+            temp = temp->next;
+        }
+    } while (swapped);
 }
 
-// Funkce pro smazani (dealokaci) seznamu
+// Funkce pro smazání (dealokaci) celého seznamu
 void deleteList(Node **head)
 {
+    Node *current = *head;
+    while (current != nullptr)
+    {
+        Node *next = current->next;
+        delete current;
+        current = next;
+    }
+    *head = nullptr;
 }
 
-// Operátor pro tisk dat
+// Operátor pro tisk dat seznamu
 std::ostream &operator<<(std::ostream &os, Node *head)
 {
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        os << temp->data << " ";
+        temp = temp->next;
+    }
     return os;
 }
 
