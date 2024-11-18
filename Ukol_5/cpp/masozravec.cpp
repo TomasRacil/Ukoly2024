9e5fd6c44b14857d1f67e919cfd1fc70f3e6513a
Jakub Václav Flasar <jakub@flasar.net>
2024-11-18 12:03:57 +0100

    Ukol 5: rozdelit do cpp/h

---
+++ Ukol_5/cpp/masozravec.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Jakub Václav Flasar on 18.11.2024.
+//
+
+#include "masozravec.h"
+#include "prostredi.h"
+#include <random>
+
+void Masozravec::rozmnozovani()
+{
+    if (energie > 500)
+    {
+        int pocetPotomku = rand() % 2;
+
+        for (int i = 0; i < pocetPotomku; ++i)
+        {
+            prostredi->pridejOrganismus<Masozravec>(x, y);
+            energie /= 2;
+        }
+    }
+}
+
+char Masozravec::getTyp() { return 'M'; }
+
+void Masozravec::konzumuj(Organismus *other) {
+    if (other->getTyp() == 'B' && energie > other->energie)
+    {
+        energie += other->energie;
+        other->energie = 0;
+    }
+}
+
+Masozravec::Masozravec(int x, int y, Prostredi *prostredi) : Zvire(x, y, prostredi)
+{
+    energie = 200;
+}
+
+
---
+++ Ukol_5/cpp/rostlina.h
@@ -0,0 +1,26 @@
+//
+// Created by Jakub Václav Flasar on 18.11.2024.
+//
+
+#ifndef UKOL_5_ROSTLINA_H
+#define UKOL_5_ROSTLINA_H
+
+#include "organismus.h"
+class Prostredi;
+
+class Rostlina : public Organismus
+{
+public:
+    Rostlina(int x, int y, Prostredi *prostredi);
+
+    void pohyb() override;
+
+    void metabolismus() override;
+
+    void rozmnozovani() override;
+
+    void konzumuj(Organismus *other) override;
+    char getTyp() override;
+};
+
+#endif //UKOL_5_ROSTLINA_H
