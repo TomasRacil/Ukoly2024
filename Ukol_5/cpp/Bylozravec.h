a4f98d15bb9cf32bc471ba6db75938103990f028
SickSkater <94305299+SickSkater@users.noreply.github.com>
2024-11-21 18:37:26 +0100

    Add files via upload
    test ukol 5 pokus

---
+++ Ukol_5/cpp/Bylozravec.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef BYLOZRAVEC_H
+#define BYLOZRAVEC_H
+
+#include "Zvire.h"
+
+class Bylozravec : public Zvire
+{
+public:
+	Bylozravec(int xP, int yP, float energyP, Type type);
+};
+
+#endif
+
+
---
+++ Ukol_5/cpp/Organismus.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#ifndef ORGANISMUS_H
+#define ORGANISMUS_H
+
+#include "iostream"
+#include <time.h>
+#include <random>
+
+class Prostredi;
+
+enum Type
+{
+	VEGETATION = 'V',
+	CARNIVORE = 'C',
+	HERBIVORE = 'H'
+};
+
+struct Position {
+	int x;
+	int y;
+};
+class Organismus
+{
+public:
+	Position pos;
+	float energy;
+	Type type;
+
+	Organismus(int xP, int yP, float energyP, Type typeP);
+	virtual void move() = 0;
+	bool isAlive();
+	float getEnergy();
+	Position getPosition();
+	bool inRange(Organismus* org);
+
+};
+
+
+#endif // !ORGANISMUS_H
