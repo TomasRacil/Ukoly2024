8b922a7a2e82d3a14a431c407de1610a9c6e29b0
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2024-11-23 15:37:44 +0100

    Initial functionality

--- Ukol_6/cpp/hanoi.cpp
+++ Ukol_6/cpp/hanoi.cpp
@@ -10,23 +10,109 @@ struct Tah
     char z;
     char na;
     vector<vector<int>> stavVezi; // Člen pro uložení stavu věží po provedení tahu
+
+    //constructor
+    Tah(int disk, char z, char na, vector<vector<int>> stavVezi) : disk(disk), z(z), na(na), stavVezi(stavVezi) {};
 };
 
-// Funkce pro provedení tahu
+// Funkce pro provedení tahu (?? pointless)
 void provedTah(vector<vector<int>> &veze, Tah tah)
 {
-    // Doplňte implementaci
+    //provede presunuti mensiho disku na vetsi smerem doprava
 }
 
 // Funkce pro řešení Hanoiských věží (bez výpisu)
 void hanoi(int n, char z, char pomocny, char cil, vector<vector<int>> &veze, vector<Tah> &tahy)
 {
-    // Doplňte implementaci
+    if (n == 0) {
+        return;
+    }
+
+    //transfer disc from start to auxiliary via end
+    hanoi(n-1, z      ,cil  ,pomocny,veze,tahy);
+
+    //move disc from z tower to cil tower
+    int from_idx;
+    switch(z) {
+        case 'A':
+            from_idx = 0;
+            break;
+        case 'B':
+            from_idx = 1;
+            break;
+        case 'C':
+            from_idx = 2;
+            break;
+    }
+    int to_idx;
+    switch(cil) {
+        case 'A':
+            to_idx = 0;
+            break;
+        case 'B':
+            to_idx = 1;
+            break;
+        case 'C':
+            to_idx = 2;
+            break;
+    }
+    veze[to_idx].push_back(veze[from_idx].back());
+    veze[from_idx].pop_back();
+
+    //push back current state of discs and operation to tahy
+    tahy.push_back(Tah(n,z,cil,veze));
+
+    //transfer disc from auxiliary to end via start
+    hanoi(n-1, pomocny,z    ,cil    ,veze,tahy);
 }
 
 void zobrazVeze(vector<vector<int>> &veze)
 {
-    // Doplňte implementaci
+    if (veze.empty()) {
+        //cannot print towers when there are no towers
+        return;
+    }
+
+    //get number of discs:
+    int discs_cnt = veze[0].size() + veze[1].size() + veze[2].size();
+
+    //print towers from top to bottom
+    //the widest tower is discs_cnt*2 + 1 wide
+    //all towers are discs_cnt + 1 high
+
+    //for all rows:
+    for (int i = discs_cnt;i>=0;--i) {
+        //for all columns (towers):
+        for(int j=0;j<3;++j) {
+            //print disc if there is any on current row:
+            if (veze[j].size() > i) {
+                //spaces_cnt are spaces needed before and after the disc, depending on disc's width
+                int spaces_cnt = discs_cnt - veze[j][i] + 1;
+                //print spaces before disc:
+                for (int k=0;k<spaces_cnt;++k)
+                    cout << " ";
+                //print disc
+                for (int k = 0;k<veze[j][i]*2+1;++k)
+                    cout << "=";
+                //print spaces after disc:
+                for (int k=0;k<spaces_cnt;++k)
+                    cout << " ";
+            }
+            //print empty spot
+            else {
+                //print spaces before '|'
+                for(int k=0;k<discs_cnt+1;++k)
+                    cout << " ";
+                cout << "|";
+                //print spaces after '|'
+                for(int k=0;k<discs_cnt+1;++k)
+                    cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+
 }
 
 #ifndef __TEST__
@@ -36,6 +122,7 @@ int main()
     cout << "Zadejte počet disků: ";
     cin >> n;
     cin.ignore();
+
 
     vector<vector<int>> veze(3);
     for (int i = n; i > 0; i--)
