8add1597f270461ee26f4c060596d6e4aede7ffa
otakarszaffner <75946054+otakarszaffner@users.noreply.github.com>
2024-11-25 08:07:31 +0100

    Update source.cpp
    dodal jsem nejake akce do source, myslel jsem ze to chcete prazdne

--- Ukol_5/cpp/source.cpp
+++ Ukol_5/cpp/source.cpp
@@ -7,5 +7,32 @@
 
 int main()
 {
+    
+    Prostredi prostredi(20, 20);
+
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+    prostredi.pridejOrganismus<Rostlina>();
+
+    prostredi.pridejOrganismus<Bylozravec>();
+    prostredi.pridejOrganismus<Bylozravec>();
+    prostredi.pridejOrganismus<Bylozravec>();
+    prostredi.pridejOrganismus<Bylozravec>();
+
+    prostredi.pridejOrganismus<Masozravec>();
+    prostredi.pridejOrganismus<Masozravec>();
+
+    prostredi.vypisStav();
+
+    for (int i = 0; i < 50; ++i)
+    {
+        prostredi.krok();
+        prostredi.vypisStav();
+    }
     return 0;
 }
