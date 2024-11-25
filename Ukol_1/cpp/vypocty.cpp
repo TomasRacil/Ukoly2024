0156f79e6a36aaf2192d843a9796ac2c109b685c
otakarszaffner <75946054+otakarszaffner@users.noreply.github.com>
2024-11-25 08:22:42 +0100

    Update vypocty.cpp

--- Ukol_1/cpp/vypocty.cpp
+++ Ukol_1/cpp/vypocty.cpp
@@ -1,46 +1,99 @@
-// #include "vypocty.h"
 #include <iostream>
 #include <vector>
+#include <sstream>
 
-int soucet(const std::vector<int> &cisla)
+int soucet(const vector<int> &cisla)
 {
-    // TODO: Implementujte funkci
-    return 0;
+    int sct = 0;
+    for (int i : cisla) {
+        sct += i;
+    }
+    try {
+        if (cisla.size() == 0) {
+            throw exception();
+        }
+    } catch (exception) {
+        return 0;
+    }
+    return sct;
 }
 
-int soucin(const std::vector<int> &cisla)
+int soucin(const vector<int> &cisla)
 {
-    // TODO: Implementujte funkci
-    return 0;
+    int sc = 1;
+    for (int i : cisla) {
+        sc *= i;
+    }
+    try {
+        if (cisla.size() == 0) {
+            throw exception();
+        }
+    } catch (exception) {
+        return 0;
+    }
+    return sc;
 }
 
-double prumer(const std::vector<int> &cisla)
+double prumer(const vector<int> &cisla)
 {
-    // TODO: Implementujte funkci
-    return 0;
+    try {
+        if (cisla.size() == 0) {
+            throw exception();
+        }
+    } catch (exception) {
+        cout << "Prazdny vstup." << endl;
+        return {};
+    }
+    double avg = 0;
+    for (int i : cisla) {
+        avg += i;
+    }
+    avg = avg / cisla.size();
+
+    return avg;
 }
 
-double median(const std::vector<int> &cisla)
+double median(const vector<int> &cisla)
 {
-    // TODO: Implementujte funkci
-    return 0;
+    try {
+        if (cisla.size() == 0) {
+            throw exception();
+        }
+    } catch (exception) {
+        cout << "Prazdny vstup." << endl;
+        return {};
+    }
+    double med = 0;
+    if (cisla.size() % 2 == 0) {
+        med = (cisla[cisla.size() / 2 - 1] + cisla[cisla.size() / 2]) / 2;
+    } else {
+        med = cisla[cisla.size() / 2];
+    }
+
+    return med;
 }
 
-#ifndef __TEST__ // Add this preprocessor guard
+using namespace std;
+#ifndef __TEST__ 
 int main()
 {
-    std::cout << "Zadejte seznam čísel oddělených čárkou: ";
-    std::string vstup;
-    std::getline(std::cin, vstup);
+    cout << "Zadejte seznam cisel oddelených carkou: ";
+    string vstup;
+    getline(cin, vstup);
+    vector<int> cisla;
+    stringstream ss(vstup);
+    string temp;
+    if(vstup.find_first_not_of("0123456789,") != string::npos){
+        cout << "Nezadal jste cislo." << endl;
+        return 1;
+    }
+    while (getline(ss, temp, ',')) {
+        cisla.push_back(stoi(temp));
+    }
 
-    std::vector<int> cisla;
-    // TODO: Načtěte čísla ze vstupu do vektoru cisla
 
-    std::cout << "Součet: " << soucet(cisla) << std::endl;
-    std::cout << "Součin: " << soucin(cisla) << std::endl;
-    std::cout << "Průměrná hodnota: " << prumer(cisla) << std::endl;
-    std::cout << "Medián: " << median(cisla) << std::endl;
+    cout << "Soucet: " << soucet(cisla) << endl << "Soucin: " << soucin(cisla) << endl << "Prumer: " << prumer(cisla) << endl << "Median: " << median(cisla) << endl;
 
     return 0;
 }
-#endif // __TEST__
+#endif
