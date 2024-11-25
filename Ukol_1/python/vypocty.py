d1490209c536a36fcf2da21c9bf8609cbc7beb1c
otakarszaffner <75946054+otakarszaffner@users.noreply.github.com>
2024-11-25 08:30:08 +0100

    Update vypocty.py
    opraveny

--- Ukol_1/python/vypocty.py
+++ Ukol_1/python/vypocty.py
@@ -1,48 +1,45 @@
+import math
+
 def soucet(cisla: list[int]) -> int:
-  soucet = 0
-  for i in range(len(cisla)):
-    soucet += cisla[i]
-  
-  return soucet
+  if not cisla:
+    print("Seznam čísel je prázdný.")
+    return 0
+  return sum(cisla)
 
 def soucin(cisla: list[int]) -> int:
-  soucin = 1
-  for i in range(len(cisla)):
-    soucin *= cisla[i]
-  
-  return soucin
-
+  if not cisla:
+    print("Seznam čísel je prázdný.")
+    return 0
+  return math.prod(cisla)
 
 def prumer(cisla: list[int]) -> float:
-  soucet = 0
-  pocet = 0
-  for i in range(len(cisla)):
-    soucet += cisla[i]
-    pocet += 1
-  prum = float(soucet/pocet)
-  
-  return prum
-
+  if not cisla:
+    cisla = []
+    raise ValueError("Seznam čísel je prázdný.")
+  return sum(cisla) / len(cisla)
 
 def median(cisla: list[int]) -> float:
-  delka = len(cisla)
   cisla.sort()
-  if(delka % 2 == 0):
-    return (cisla[delka//2 - 1] + cisla[delka//2]) / 2
-  else:
-    return cisla[delka//2]
+  if not cisla:
+    cisla = []
+    raise ValueError("Seznam čísel je prázdný.")
 
+  delka = len(cisla)
+  if delka % 2 == 0:
+    return (cisla[delka // 2 - 1] + cisla[delka // 2]) / 2
+  else:
+    return cisla[delka // 2]
 
 def main():
-    """Načte vstup od uživatele, zavolá funkce pro výpočet a vypíše výsledky."""
-    vstup = input("Zadejte seznam čísel oddělených čárkou: ")
-    cisla = [int(cislo) for cislo in vstup.split(",")]
+  vstup = input("Zadejte seznam čísel oddělených čárkou: ")
+  cisla = [int(cislo) for cislo in vstup.split(",") if cislo.strip() != "" and cislo.strip().isdigit()]
+  if not cisla:
+    raise ValueError("Nebyla zadána žádná čísla.")  #
 
-    print("Součet:", soucet(cisla))
-    print("Součin:", soucin(cisla))
-    print("Průměrná hodnota:", prumer(cisla))
-    print("Medián:", median(cisla))
-
+  print("Součet:", soucet(cisla))
+  print("Součin:", soucin(cisla))
+  print("Průměrná hodnota:", prumer(cisla))
+  print("Medián:", median(cisla))
 
 if __name__ == "__main__":
-    main()
+  main()
