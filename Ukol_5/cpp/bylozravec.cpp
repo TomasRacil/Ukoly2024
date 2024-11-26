e5becb67f8ea6bb4791e3c6407979b08353921e4
Storm788 <krystof.pavlu@unob.cz>
2024-11-26 16:21:07 +0100

    Update bylozravec.cpp

--- Ukol_5/cpp/bylozravec.cpp
+++ Ukol_5/cpp/bylozravec.cpp
@@ -1,1 +1,33 @@
+#include "bylozravec.h"
+#include "prostredi.h"
+#include <random>
+
+void Bylozravec::rozmnozovani()
+{
+    if (energie > 300)
+    {
+        int pocetPotomku = rand() % 4;
+
+        for (int i = 0; i < pocetPotomku; ++i)
+        {
+            prostredi->pridejOrganismus<Bylozravec>(x, y);
+            energie /= 2;
+        }
+    }
+}
+
+Bylozravec::Bylozravec(int x, int y, Prostredi *prostredi) : Zvire(x, y, prostredi)
+{
+    energie = 100;
+}
+
+char Bylozravec::getTyp() { return 'B'; }
+
+void Bylozravec::konzumuj(Organismus *other) {
+    if (other->getTyp() == 'R' && energie > other->energie)
+    {
+        energie += other->energie;
+        other->energie = 0;
+    }
+}
 
