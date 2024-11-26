a1dd4cac14793a2e4f5261426924f130a6dfaf4c
Storm788 <krystof.pavlu@unob.cz>
2024-11-26 16:37:34 +0100

    Update rostlina.h

--- Ukol_5/cpp/rostlina.h
+++ Ukol_5/cpp/rostlina.h
@@ -1,1 +1,22 @@
+#ifndef UKOL_5_ROSTLINA_H
+#define UKOL_5_ROSTLINA_H
 
+#include "organismus.h"
+class Prostredi;
+
+class Rostlina : public Organismus
+{
+public:
+    Rostlina(int x, int y, Prostredi *prostredi);
+
+    void pohyb() override;
+
+    void metabolismus() override;
+
+    void rozmnozovani() override;
+
+    void konzumuj(Organismus *other) override;
+    char getTyp() override;
+};
+
+#endif
