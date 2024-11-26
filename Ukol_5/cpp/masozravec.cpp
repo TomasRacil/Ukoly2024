98c299dcf901a5dcd3bf2df526e3ec2c62e84c8a
Storm788 <krystof.pavlu@unob.cz>
2024-11-26 16:36:10 +0100

    Update masozravec.cpp

--- Ukol_5/cpp/masozravec.cpp
+++ Ukol_5/cpp/masozravec.cpp
@@ -1,1 +1,35 @@
+#include "masozravec.h"
+#include "prostredi.h"
+#include <random>
+
+void Masozravec::rozmnozovani()
+{
+    if (energie > 500)
+    {
+        int pocetPotomku = rand() % 2;
+
+        for (int i = 0; i < pocetPotomku; ++i)
+        {
+            prostredi->pridejOrganismus<Masozravec>(x, y);
+            energie /= 2;
+        }
+    }
+}
+
+char Masozravec::getTyp() { return 'M'; }
+
+void Masozravec::konzumuj(Organismus *other) {
+    if (other->getTyp() == 'B' && energie > other->energie)
+    {
+        energie += other->energie;
+        other->energie = 0;
+    }
+}
+
+Masozravec::Masozravec(int x, int y, Prostredi *prostredi) : Zvire(x, y, prostredi)
+{
+    energie = 200;
+}
+
+
 
