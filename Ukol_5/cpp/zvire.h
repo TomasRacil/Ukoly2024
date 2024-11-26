72ca72822b84255a3fa3f7db27c51d2d9c4fe30e
Storm788 <krystof.pavlu@unob.cz>
2024-11-26 16:38:10 +0100

    Update zvire.h

--- Ukol_5/cpp/zvire.h
+++ Ukol_5/cpp/zvire.h
@@ -1,1 +1,17 @@
+#ifndef UKOL_5_ZVIRE_H
+#define UKOL_5_ZVIRE_H
 
+#include "organismus.h"
+
+class Zvire : public Organismus
+{
+public:
+    Zvire(int x, int y, Prostredi *prostredi) : Organismus(x, y, prostredi) {}
+
+    void pohyb() override;
+    void metabolismus() override;
+    virtual void rozmnozovani() = 0;
+    virtual void konzumuj(Organismus *other) = 0;
+};
+
+#endif
