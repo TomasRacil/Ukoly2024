221bd4907ea403caa337d375882ce4a9b02953b5
johankak <Johanka-k@email.cz>
2024-11-27 18:27:05 +0100

    hanoi opraveno

--- Ukol_6/cpp/hanoi.cpp
+++ Ukol_6/cpp/hanoi.cpp
@@ -27,6 +27,10 @@ void hanoi(int n, char z, char pomocny, char cil, vector<vector<int>>& veze, vec
     if (n <= 0) {
         return;
     }
+    if (veze[0].empty() && veze[1].empty() && veze[2].empty())
+      for(int i = n; i > 0; i--)
+        veze[0].push_back(i);
+
 
 
     // Rekurzivní řešení pro n-1
