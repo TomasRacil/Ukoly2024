91306f9d685c255dded09707c6e4980ed08151b2
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2024-11-27 21:00:54 +0100

    fixed multiplication

--- Ukol_7/python/matice.py
+++ Ukol_7/python/matice.py
@@ -5,6 +5,7 @@ def vytvor_matici(n: int, m: int) -> list[list[int]]:
     """Vytvoří matici n x m s náhodnými celými čísly od 0 do 9."""
     matice: list[list[int]] = [[ randrange(0,9) for element in range(m)] for row in range(n)]
     return matice
+            
 
 
 def reprezentace_matice(matice: list[list[int]]) -> str:
@@ -23,41 +24,50 @@ def reprezentace_matice(matice: list[list[int]]) -> str:
 
 def soucet_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[list[int]]:
     """Sečte dvě matice, pokud mají stejné rozměry."""
-    if len(matice1) == len(matice2) and len(matice1[0]) == len(matice2[0]):
+    if len(matice1) == len(matice2) and len(matice1[0]) == len(matice2[0] and len(matice) > 0):
         return [[matice1[i][j] + matice2[i][j] for j in range(len(matice1[0]))] for i in range(len(matice1))]
     return None
 
 
 def nasobeni_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[list[int]]:
     """Vynásobí dvě matice, pokud je násobení proveditelné."""
-    if len(matice1[0]) == len(matice2):
-        return [[matice1[i][j] * matice2[j][i] for j in range(len(matice1[0]))] for i in range(len(matice1))]
-    return None
+    #A(nm) B(uv) are matrixes with dimensions n*m and u*v
+    #m = u
+    if len(matice1[0]) != len(matice2):
+        return None
+
+    #result of AB = R(nv)
+    R = [[0 for elements in range(len(matice2[0]))] for row in range(len(matice1))]
+
+    #R(ij) = A(i0)B(0j) + A(i1)B(1j) + ... + A(im)B(uj), m=u
+    for i in range(len(R)):
+        for j in range(len(R[0])):
+
+            for k in range(len(matice2)):
+                R[i][j] += matice1[i][k] * matice2[k][j]
+    
+    return R
+            
+
 
 
 def transpozice_matice(matice: list[list[int]]) -> list[list[int]]:
     """Provede transpozici matice."""
-    matice: list[list[int]] = [[matice[j][i] for j in range(len(matice))] for i in range(len(matice[0]))]
-    return matice
+    if len(matice) > 0:
+        return [[matice[j][i] for j in range(len(matice[0]))] for i in range(len(matice))]
+    return []
 
 
 if __name__ == "__main__":
-    matice1: list[list[int]] = vytvor_matici(3, 2)
-    matice2: list[list[int]] = vytvor_matici(2, 4)
+    matice1 = [[1, 2], [3, 4]]
+    matice2 = [[5, 6], [7, 8]]
+    expected_product = [[19, 22], [43, 50]]
+    #self.assertEqual(nasobeni_matic(matice1, matice2), expected_product)
 
-    print("Matice 1:")
-    print(reprezentace_matice(matice1))
-    print("Matice 2:")
-    print(reprezentace_matice(matice2))
+    matice3 = [[1, 2], [3, 4], [5, 6]]
+    #self.assertIsNone(nasobeni_matic(matice1, matice3))
+    #self.assertIsNone(nasobeni_matic(matice3, matice1))
 
-    soucet = soucet_matic(matice1, matice1)  # Sečteme matici1 samu se sebou
-    print("Součet matic:")
-    print(reprezentace_matice(soucet))
-
-    nasobek = nasobeni_matic(matice1, matice2)
-    print("Násobení matic:")
-    print(reprezentace_matice(nasobek))
-
-    transponovana = transpozice_matice(matice1)
-    print("Transponovaná matice:")
-    print(reprezentace_matice(transponovana))
+    #self.assertEqual(nasobeni_matic([], []), [])
+    #self.assertIsNone(nasobeni_matic(matice1, []))
+    #self.assertIsNone(nasobeni_matic([], matice1))
