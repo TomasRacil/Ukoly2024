3f17be204223f5dea71346b30e19aeccebf616a3
otakarszaffner <75946054+otakarszaffner@users.noreply.github.com>
2024-12-01 14:29:51 +0100

    Update matice.py
    oprava1

--- Ukol_7/python/matice.py
+++ Ukol_7/python/matice.py
@@ -11,14 +11,20 @@ def vytvor_matici(n: int, m: int) -> list[list[int]]:
 
 def reprezentace_matice(matice: list[list[int]]) -> str:
     """Vrátí stringovou reprezentaci matice."""
-    return '\n'.join(' '.join(map(str, row)) for row in matice)
+    if matice:
+        str_mat = ""
+        for radek in matice:
+            str_mat += " ".join(map(str, radek)) + "\n"
+        return str_mat
+    else: return ""
 
 
 def soucet_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[list[int]]:
     """Sečte dvě matice, pokud mají stejné rozměry."""
+    if not matice1 and not matice2: return []
     matice: list[list[int]] = []
     if len(matice1) != len(matice2) or any(len(radek1) != len(radek2) for radek1, radek2 in zip(matice1, matice2)):
-        return "Nelze scitat"
+        return None
     
     rows = len(matice1)
     cols = len(matice1[0])
@@ -30,9 +36,10 @@ def soucet_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[lis
 
 def nasobeni_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[list[int]]:
     """Vynásobí dvě matice, pokud je násobení proveditelné."""
+    if not matice1 or matice2: return []
     matice: list[list[int]] = []
     if len(matice1[0]) != len(matice2):
-        return "Nelze nasobit"
+        return None
     pocet_radku = len(matice1)
     pocet_sloupcu = len(matice2[0])
     spolecna_dim = len(matice2)
@@ -48,12 +55,14 @@ def nasobeni_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[l
 
 def transpozice_matice(matice: list[list[int]]) -> list[list[int]]:
     """Provede transpozici matice."""
+    if not matice: return []
+    if not matice[0]: return [[]]
     matice: list[list[int]] = [[matice[j][i] for j in range(len(matice))] for i in range(len(matice[0]))]
     return matice
 
 
 if __name__ == "__main__":
-    matice1: list[list[int]] = vytvor_matici(3, 2)
+    matice1: list[list[int]] = vytvor_matici(0, 0)
     matice2: list[list[int]] = vytvor_matici(2, 4)
 
     print("Matice 1:")
