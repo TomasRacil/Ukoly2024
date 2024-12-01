1df9e5d2e28a7a1ec943987049e4a0a644f93297
vojtechvel <vojtechvel@gmail.com>
2024-12-01 19:40:57 +0100

    Update vypocty.py

--- Ukol_1/python/vypocty.py
+++ Ukol_1/python/vypocty.py
@@ -1,45 +1,38 @@
-import math
+import statistics
 
 def soucet(cisla: list[int]) -> int:
-  if not cisla:
-    print("Seznam čísel je prázdný.")
-    return 0
   return sum(cisla)
 
+
 def soucin(cisla: list[int]) -> int:
-  if not cisla:
-    print("Seznam čísel je prázdný.")
-    return 0
-  return math.prod(cisla)
+  if len(cisla) == 0:
+     return 0
+  soucin = 1
+  for i in cisla:
+    soucin *= i
+  return soucin
+
 
 def prumer(cisla: list[int]) -> float:
-  if not cisla:
-    cisla = []
-    raise ValueError("Seznam čísel je prázdný.")
+  if len(cisla) == 0:
+    raise ValueError
   return sum(cisla) / len(cisla)
 
-def median(cisla: list[int]) -> float:
-  cisla.sort()
-  if not cisla:
-    cisla = []
-    raise ValueError("Seznam čísel je prázdný.")
 
-  delka = len(cisla)
-  if delka % 2 == 0:
-    return (cisla[delka // 2 - 1] + cisla[delka // 2]) / 2
-  else:
-    return cisla[delka // 2]
+def median(cisla: list[int]) -> float:
+    return statistics.median(cisla)
+
 
 def main():
-  vstup = input("Zadejte seznam čísel oddělených čárkou: ")
-  cisla = [int(cislo) for cislo in vstup.split(",") if cislo.strip() != "" and cislo.strip().isdigit()]
-  if not cisla:
-    raise ValueError("Nebyla zadána žádná čísla.")  #
+    """Načte vstup od uživatele, zavolá funkce pro výpočet a vypíše výsledky."""
+    vstup = input("Zadejte seznam čísel oddělených čárkou: ")
+    cisla = [int(cislo) for cislo in vstup.split(",")]
 
-  print("Součet:", soucet(cisla))
-  print("Součin:", soucin(cisla))
-  print("Průměrná hodnota:", prumer(cisla))
-  print("Medián:", median(cisla))
+    print("Součet:", soucet(cisla))
+    print("Součin:", soucin(cisla))
+    print("Průměrná hodnota:", prumer(cisla))
+    print("Medián:", median(cisla))
+
 
 if __name__ == "__main__":
-  main()
+    main()
