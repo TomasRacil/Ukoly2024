bbcc9e64b1bbaf789b4fa44a27e326827c3a0748
vojtechvel <vojtechvel@gmail.com>
2024-12-01 19:40:28 +0100

    Update vypocty.cpp

--- Ukol_1/cpp/vypocty.cpp
+++ Ukol_1/cpp/vypocty.cpp
@@ -1,114 +1,61 @@
+// #include "vypocty.h"
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <bits/stdc++.h>
+#include <algorithm>
 
-using namespace std;
-
-void sort_nums(vector<int> &cisla) {
-    for (size_t i = 0; i < cisla.size(); i++) {
-        for (size_t j = 0; j < cisla.size() - i - 1; j++) {
-            if (cisla[j] > cisla[j + 1]) {
-                int tmp = cisla[j];
-                cisla[j] = cisla[j + 1];
-                cisla[j + 1] = tmp;
-            }
-        }
-    }
-}
-
-int soucet(const vector<int> &cisla)
+int soucet(const std::vector<int> &cisla)
 {
-    int sct = 0;
-    for (int i : cisla) {
-        sct += i;
+    int sum = 0;
+    for (auto i : cisla) {
+        sum += i;
     }
-    try {
-        if (cisla.size() == 0) {
-            throw exception();
-        }
-    } catch (exception) {
-        return 0;
-    }
-    return sct;
+    return sum;
 }
 
-int soucin(const vector<int> &cisla)
+int soucin(const std::vector<int> &cisla)
 {
-    int sc = 1;
-    for (int i : cisla) {
-        sc *= i;
+    int sum = 1;
+    for (auto i : cisla) {
+        sum *= i;
     }
-    try {
-        if (cisla.size() == 0) {
-            throw exception();
-        }
-    } catch (exception) {
-        return 0;
-    }
-    return sc;
+    return sum;
 }
 
-double prumer(const vector<int> &cisla)
+double prumer(const std::vector<int> &cisla)
 {
-    try {
-        if (cisla.size() == 0) {
-            throw exception();
-        }
-    } catch (exception) {
-        cout << "Prazdny vstup." << endl;
-        return {};
-    }
-    double avg = 0;
-    for (int i : cisla) {
-        avg += i;
-    }
-    avg = avg / cisla.size();
-
-    return avg;
+    return soucet(cisla)/cisla.size();
 }
 
-double median(const vector<int> &cisla)
+double median(std::vector<int> &cisla)
 {
-    try {
-        if (cisla.size() == 0) {
-            throw exception();
-        }
-    } catch (exception) {
-        cout << "Prazdny vstup." << endl;
-        return {};
+    std::sort(cisla.begin(), cisla.end());
+    if (cisla.size()%2){
+        return cisla[cisla.size()/2];
     }
-    double med = 0;
-    vector<int> tmp = cisla;
-    sort_nums(tmp);
-    if (tmp.size() % 2 == 0) {
-        med = (tmp[tmp.size() / 2 - 1] + tmp[tmp.size() / 2]) / 2;
-    } else {
-        med = tmp[tmp.size() / 2];
-    }
-
-    return med;
+    return (cisla[cisla.size()/2] + cisla[cisla.size()/2 - 1])/(double)2;
 }
 
-#ifndef __TEST__ 
+#ifndef __TEST__ // Add this preprocessor guard
 int main()
 {
-    cout << "Zadejte seznam cisel oddelených carkou: ";
-    string vstup;
-    getline(cin, vstup);
-    vector<int> cisla;
-    stringstream ss(vstup);
-    string temp;
-    if(vstup.find_first_not_of("0123456789,") != string::npos){
-        cout << "Nezadal jste cislo." << endl;
-        return 1;
-    }
-    while (getline(ss, temp, ',')) {
-        cisla.push_back(stoi(temp));
+    std::cout << "Zadejte seznam čísel oddělených čárkou: ";
+    std::string vstup;
+    std::getline(std::cin, vstup);
+
+    std::vector<int> cisla;
+    std::stringstream ss(vstup);
+    while(ss.good()) {
+        std::string substr;
+        std::getline(ss, substr, ',');
+        cisla.push_back(std::stoi(substr));
     }
 
-    cout << "Soucet: " << soucet(cisla) << endl << "Soucin: " << soucin(cisla) << endl << "Prumer: " << prumer(cisla) << endl << "Median: " << median(cisla) << endl;
+    std::cout << "Součet: " << soucet(cisla) << std::endl;
+    std::cout << "Součin: " << soucin(cisla) << std::endl;
+    std::cout << "Průměrná hodnota: " << prumer(cisla) << std::endl;
+    std::cout << "Medián: " << median(cisla) << std::endl;
 
     return 0;
-
 }
-#endif
+#endif // __TEST__
