668f8f616c9dc019cdd789d216d3d7a22b0c7958
Storm788 <krystof.pavlu@unob.cz>
2024-12-03 14:18:00 +0100

    Update hanoi.cpp

--- Ukol_6/cpp/hanoi.cpp
+++ Ukol_6/cpp/hanoi.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <locale>
+#include <codecvt>
 
 using namespace std;
 
 // Struktura pro reprezentaci tahu
-struct Tah
-{
+struct Tah {
     int disk;
     char z;
     char na;
@@ -13,33 +15,66 @@ struct Tah
 };
 
 // Funkce pro provedení tahu
-void provedTah(vector<vector<int>> &veze, Tah tah)
-{
-    // Doplňte implementaci
+void provedTah(vector<vector<int>> &veze, Tah tah) {
+    int disk = veze[tah.z - 'A'].back();
+    veze[tah.z - 'A'].pop_back();
+    veze[tah.na - 'A'].push_back(disk);
+    tah.stavVezi = veze;
 }
 
 // Funkce pro řešení Hanoiských věží (bez výpisu)
-void hanoi(int n, char z, char pomocny, char cil, vector<vector<int>> &veze, vector<Tah> &tahy)
-{
-    // Doplňte implementaci
+void hanoi(int n, char z, char pomocny, char cil, vector<vector<int>> &veze, vector<Tah> &tahy) {
+    if (n == 1) {
+        Tah tah = {n, z, cil, veze};
+        provedTah(veze, tah);
+        tahy.push_back(tah);
+    } else {
+        hanoi(n - 1, z, cil, pomocny, veze, tahy);
+        Tah tah = {n, z, cil, veze};
+        provedTah(veze, tah);
+        tahy.push_back(tah);
+        hanoi(n - 1, pomocny, z, cil, veze, tahy);
+    }
 }
 
-void zobrazVeze(vector<vector<int>> &veze)
-{
-    // Doplňte implementaci
+// Funkce pro zobrazení věží
+void zobrazVeze(const vector<vector<int>> &veze) {
+    int max_height = 0;
+    for (const auto &veze : veze) {
+        max_height = max(max_height, (int)veze.size());
+    }
+
+    for (int i = max_height - 1; i >= 0; --i) {
+        for (const auto &veze : veze) {
+            if (i < veze.size()) {
+                cout << string(max_height - veze[i], ' ') << string(veze[i], '=') << string(max_height - veze[i], ' ') << " ";
+            } else {
+                cout << string(max_height, ' ') << "|" << string(max_height, ' ') << " ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
 #ifndef __TEST__
-int main()
-{
+int main() {
+    // Set locale to UTF-8
+    locale::global(locale(locale(), new codecvt_utf8<wchar_t>));
+    wcout.imbue(locale());
+
     int n;
-    cout << "Zadejte počet disků: ";
+    wcout << L"Zadejte počet disků: ";
     cin >> n;
     cin.ignore();
 
+    if (n <= 0) {
+        wcout << L"Neplatný počet disků." << endl;
+        return 1;
+    }
+
     vector<vector<int>> veze(3);
-    for (int i = n; i > 0; i--)
-    {
+    for (int i = n; i > 0; i--) {
         veze[0].push_back(i);
     }
 
@@ -47,9 +82,8 @@ int main()
     hanoi(n, 'A', 'B', 'C', veze, tahy);
 
     // Zobrazení tahů a stavů věží
-    for (Tah tah : tahy)
-    {
-        cout << "Přesuň disk " << tah.disk << " z kolíku " << tah.z << " na kolík " << tah.na << endl;
+    for (const Tah &tah : tahy) {
+        wcout << L"Přesuň disk " << tah.disk << L" z kolíku " << tah.z << L" na kolík " << tah.na << endl;
         zobrazVeze(tah.stavVezi); // Zobrazení stavu věží po tahu
     }
 
