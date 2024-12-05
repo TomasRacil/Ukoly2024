6e8a30ab1f162de6efe51d4dfbabf3eadc088fdd
WhiteKnight <114160109+Blacki005@users.noreply.github.com>
2024-12-05 20:20:24 +0100

    idk

--- Ukol_8/python/matice.py
+++ Ukol_8/python/matice.py
@@ -5,7 +5,11 @@ from random import randrange
 
 class Matice:
     def __init__(self, n: int, m: int, data=None):
-        """Inicializuje matici n x m."""
+        self.n = n
+        self.m = m
+        if data != None:
+            self.data = data
+            return
         #Matrix A = (aij) of type n*m, where i = 1,2,3,...,n and j = 1,2,3,...,m has n rows and m columns 
         if n < 0 or m < 0:
             return None
@@ -24,30 +28,35 @@ class Matice:
                     if j < len(self.data[i]) - 1:
                         str_matrix += str(" ")
                 str_matrix += "\n"
+        #delete last newline
+        str_matrix = str_matrix[:-1]
         return str_matrix
 
     def __add__(self, other: Matice) -> Matice:
         """Sečte aktuální matici s maticí other."""
         # Implementace součtu matic
-        if len(self.data) == len(other.data):
-            if len(self.data) == 0:
+        if self.n == other.n:
+
+            if self.n == 0:
                 #both matrixes are empty
-                return []
-            if len(self.data[0]) == len(other.data[0]):
-                return [[self.data[i][j] + other.data[i][j] for j in range(len(self.data[0]))] for i in range(len(self.data))]
+                return Matice(0,0,[])
+            if self.m == other.m:
+                new_data = [[self.data[i][j] + other.data[i][j] for j in range(self.m)] for i in range(self.n)]
+                return Matice(self.n,self.m, new_data)
         return None
 
     def __mul__(self, other: Union[Matice, int]) -> Union[Matice, int]:
         """Vynásobí aktuální matici maticí nebo skalárem."""
+
         try:
             #A(nm) B(uv) are matrixes with dimensions n*m and u*v
 
-            if len(self.data) != 0:
+            if self.n != 0:
                 #check if m = u
-                if len(self.data[0]) != len(other.data):
+                if self.m != other.n:
                     #cannot multiply
                     return None
-            elif len(other.data) == 0:
+            elif other.n == 0:
                 #both matrixes are empty
                 return []
             else:
@@ -56,37 +65,40 @@ class Matice:
 
             #result of AB = R(nv)
             #init result matrix R with zeroes
-            R = [[0 for _ in range(len(other.data[0]))] for _ in range(len(self.data))]
+            R = Matice(self.n, other.m, [[0 for _ in range(other.m)] for _ in range(self.n)])
 
             #R(ij) = A(i0)B(0j) + A(i1)B(1j) + ... + A(im)B(uj)
-            for i in range(len(R)):
-                for j in range(len(R[0])):
-                    for k in range(len(other.data)):
-                        R[i][j] += self.data[i][k] * other.data[k][j]
+            for i in range(R.n):
+                for j in range(R.m):
+                    for k in range(other.n):
+                        R.data[i][j] += self.data[i][k] * other.data[k][j]
             
             return R
         except AttributeError:
             #other is int
-            R = []
+            R = Matice(self.n,self.m,[])
             for row in self.data:
                 new_row = []
                 for element in row:
                     new_row.append(element * other)
-                R.append(new_row)
+                R.data.append(new_row)
             return R
 
     def transpozice(self) -> Matice:
         """Vrátí transponovanou matici."""
         if len(self.data) > 0 and len(self.data[0]) > 0:
-            return [[self.data[j][i] for j in range(len(self.data))] for i in range(len(self.data[0]))]
+            return Matice(self.m,self.n,[[self.data[j][i] for j in range(len(self.data))] for i in range(len(self.data[0]))])
         elif len(self.data) > 0 and len(self.data[0]) == 0:
             #n rows, 0 columns
-            return [[] for i in range(len(self.data))]
+            return Matice(len(self.data),0,[[] for i in range(len(self.data))])
         #0 rows
-        return []
+        return Matice(0,0,[])
 
 
 if __name__ == "__main__":
+
+
+    
     # Vytvořte instance třídy Matice a otestujte metody
     matice1 = Matice(3, 2)
     matice2 = Matice(2, 4)
