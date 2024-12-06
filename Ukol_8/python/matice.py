fb2c4b8cecc7c5add32183488c7ce01245062a03
NikACR <holynikodem1@seznam.cz>
2024-12-06 20:13:02 +0000

    Úkol 8 - Holý

--- Ukol_8/python/matice.py
+++ Ukol_8/python/matice.py
@@ -6,29 +6,53 @@ import random
 class Matice:
     def __init__(self, n: int, m: int, data=None):
         """Inicializuje matici n x m."""
-        pass
+        self.n = n
+        self.m = m
+        if data is None:
+            self.data = [[random.randint(0, 9) for _ in range(m)] for _ in range(n)]
+        else:
+            self.data = data
 
     def __str__(self) -> str:
         """Vrátí stringovou reprezentaci matice."""
-        pass
+        return "\n".join(" ".join(map(str, row)) for row in self.data)
 
     def __add__(self, other: Matice) -> Matice:
         """Sečte aktuální matici s maticí other."""
-        # Implementace součtu matic
-        pass
+        if self.n != other.n or self.m != other.m:
+            raise ValueError("Matice musí mít stejné rozměry pro součet.")
+        
+        return Matice(self.n, self.m, [
+            [self.data[i][j] + other.data[i][j] for j in range(self.m)]
+            for i in range(self.n)
+        ])
 
     def __mul__(self, other: Union[Matice, int]) -> Union[Matice, int]:
         """Vynásobí aktuální matici maticí nebo skalárem."""
-        # Implementace násobení matic
-        pass
+        if isinstance(other, int):
+            return Matice(self.n, self.m, [
+                [self.data[i][j] * other for j in range(self.m)]
+                for i in range(self.n)
+            ])
+        
+        elif isinstance(other, Matice):
+            if self.m != other.n:
+                raise ValueError("Počet sloupců první matice musí být roven počtu řádků druhé matice.")
+            
+            return Matice(self.n, other.m, [
+                [sum(self.data[i][k] * other.data[k][j] for k in range(self.m)) for j in range(other.m)]
+                for i in range(self.n)
+            ])
 
     def transpozice(self) -> Matice:
         """Vrátí transponovanou matici."""
-        # Implementace transpozice matice
+        return Matice(self.m, self.n, [
+            [self.data[i][j] for i in range(self.n)]
+            for j in range(self.m)
+        ])
 
 
 if __name__ == "__main__":
-    # Vytvořte instance třídy Matice a otestujte metody
     matice1 = Matice(3, 2)
     matice2 = Matice(2, 4)
 
@@ -37,18 +61,18 @@ if __name__ == "__main__":
     print("Matice 2:")
     print(matice2)
 
-    soucet = matice1+matice1  # Sečteme matici1 samu se sebou
+    soucet = matice1 + matice1
     print("Součet matic:")
     print(soucet)
 
-    nasobek = matice1*matice2  # Násobujeme matice1 a matice2
+    nasobek = matice1 * matice2
     print("Násobení matic:")
     print(nasobek)
 
-    skalarni_nasobek = matice1*10
+    skalarni_nasobek = matice1 * 10
     print("Skálární násobek:")
     print(skalarni_nasobek)
 
     transponovana = matice1.transpozice()
     print("Transponovaná matice:")
-    print(transponovana),
+    print(transponovana)
