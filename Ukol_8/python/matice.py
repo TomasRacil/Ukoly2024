5b5ea565d0d7641be86d2d8585b944f57a20f164
DanielBi1ek <145032686+DanielBi1ek@users.noreply.github.com>
2024-12-09 17:21:18 +0100

    Ukol7,8 (#119)
    * Ukol 0 pridan testy rozdeleny podle ukolu.
    
    * Ukol 3 added and tests modified
    
    * Update README.md
    
    * sifry finall
    
    * ukol 1
    
    * ukol 1
    
    * Ukol 0 vyresen (#44)
    
    * Oprava nazvu testu
    
    * PostMergeFix
    
    * prazdny seznam fixed
    
    * ukol 0 c++
    
    * ukol0 python
    
    * osetreni vstupu
    
    * prazdny soubor osetreni
    
    * osetreni vstupu python
    
    * linked_list_final
    
    * linked_list_final
    
    * osetreni vkladani mezer
    
    * opravy testů
    
    * male pismeno fix
    
    * male pismeno fix
    
    * TEST sync
    
    * mezera na konci fix
    
    * raise ValueError fix
    
    * Ukol 4 přidán
    
    * Ukol 4 fix
    
    * advent_lod_Final
    
    * ukol1 try catch implementation
    
    zjistil jsem že testy to chtějí jako try a catch
    
    * ukol 1 succses test completion
    
    * succsesful test fix
    
    * Ukol 5 pridan
    
    * headers ukol final
    
    * Update testCpp.yml
    
    * Úkol 6 pridan
    
    * ukol6 final
    
    * ukol6 test fix
    
    * ukol6 test fix2
    
    * ukol6 test fix3
    
    * ukol6 test fix3
    
    * ukol6 test fix4
    
    * ukol6 test fix5
    
    * ukol6 test fix6
    
    * ukol6 test fix7:(
    
    * ukol6 test fix
    
    * ukol6 test fix
    
    * Update testCpp.yml
    
    * ukol6 FINAL_Test_FIX:)
    
    * Ukol 7 pridan
    
    * Ukol7 ver1
    
    * Ukol 8 pridan opraven test ukolu 7
    
    * Ukol7 testFix
    
    * Ukol8 final
    
    ---------
    
    Co-authored-by: tomasracil <tomas.racil@unob.cz>
    Co-authored-by: TomasRacil <72918819+TomasRacil@users.noreply.github.com>
    Co-authored-by: TandRCZ <50552034+TandRCZ@users.noreply.github.com>

---
+++ Ukol_8/python/matice.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+from typing import Union
+import random
+
+
+class Matice:
+    def __init__(self, n: int, m: int, data=None):
+        """Inicializuje matici n x m."""
+        self.n = n
+        self.m = m
+
+        if data:
+            self.data = data
+        else:
+            self.data = []
+            for i in range(n):
+                radek = []
+                for j in range(m):
+                    radek.append(random.randint(0, 9))
+                self.data.append(radek)
+
+    def __str__(self) -> str:
+        """Vrátí stringovou reprezentaci matice."""
+        return "\n".join(" ".join(map(str, radek)) for radek in self.data)
+
+    def __eq__(self, other: object) -> bool:  # bude voláno při porovnání matic. Vrací data a ne paměťovou adresu objektu
+        """Porovná aktuální matici s maticí other."""
+        if not isinstance(other, Matice):  # pokud other není instance Matice
+            return False # vrátí False
+        return self.n == other.n and self.m == other.m and self.data == other.data  # porovnání rozměrů a dat matic
+
+    def __add__(self, other: Matice) -> Matice:
+        """Sečte aktuální matici s maticí other."""
+        if len(self.data) == len(other.data) and all(len(row) == len(other.data[i]) for i, row in enumerate(self.data)): # kontrola rozměrů matic
+            matice = []
+            for i in range(len(self.data)):
+                radek = []
+                for j in range(len(self.data[i])):
+                    radek.append(self.data[i][j] + other.data[i][j]) # sečtení prvků
+                matice.append(radek)
+            return Matice(len(self.data), len(self.data[0]), matice)
+        else:
+            raise ValueError("Matice nemají stejné rozměry")
+
+    def __mul__(self, other: Union[Matice, int]) -> Union[Matice, int]:
+        """Vynásobí aktuální matici maticí nebo skalárem."""
+        if isinstance(other, Matice):  # pokud je other instance Matice -> násobení matic
+
+            if len(self.data[0]) == len(other.data):  # kontrola kompatibility matic
+                matice = []
+                for i in range(len(self.data)):
+                    radek = []
+                    for j in range(len(other.data[0])):
+                        hodnota = 0
+                        for k in range(len(self.data[0])):
+                            hodnota += self.data[i][k] * other.data[k][j]  # výpočet hodnoty prvku výsledné matice
+                        radek.append(hodnota)
+                    matice.append(radek)
+                return Matice(len(self.data), len(other.data[0]), matice)
+            else:
+                raise ValueError("Matice nejsou kompatibilní pro násobení")
+        elif isinstance(other, int): # pokud je other int -> násobení skalárem
+            matice = []
+            for i in range(len(self.data)):
+                radek = []
+                for j in range(len(self.data[i])):
+                    radek.append(self.data[i][j] * other) # násobení prvků skalárem
+                matice.append(radek)
+            return Matice(len(self.data), len(self.data[0]), matice)
+
+    def transpozice(self) -> Matice:
+        """Vrátí transponovanou matici."""
+        if not self.data or not self.data[0]:
+            return Matice(0, 0, [])
+        matice = []
+        for i in range(len(self.data[0])):
+            radek = []
+            for j in range(len(self.data)):
+                radek.append(self.data[j][i])
+            matice.append(radek)
+        return Matice(len(self.data[0]), len(self.data), matice)
+
+
+
+if __name__ == "__main__":
+    # Vytvořte instance třídy Matice a otestujte metody
+    matice1 = Matice(3, 2)
+    matice2 = Matice(2, 4)
+
+    print("Matice 1:")
+    print(matice1)
+    print("Matice 2:")
+    print(matice2)
+
+    soucet = matice1 + matice1  # Sečteme matici1 samu se sebou
+    print("Součet matic:")
+    print(soucet)
+
+    nasobek = matice1 * matice2  # Násobujeme matice1 a matice2
+    print("Násobení matic:")
+    print(nasobek)
+
+    skalarni_nasobek = matice1 * 10
+    print("Skálární násobek:")
+    print(skalarni_nasobek)
+
+    transponovana = matice1.transpozice()
+    print("Transponovaná matice:")
+    print(transponovana)
