cf473e62693fbb1ce8c878dca9b43d1ebb2ec534
otakarszaffner <75946054+otakarszaffner@users.noreply.github.com>
2024-12-10 08:44:01 +0100

    Update matice.py
    oprava funkce nasobeni_matic

--- Ukol_7/python/matice.py
+++ Ukol_7/python/matice.py
@@ -36,7 +36,7 @@ def soucet_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[lis
 
 def nasobeni_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[list[int]]:
     """Vynásobí dvě matice, pokud je násobení proveditelné."""
-    if not matice1 or not matice2: return []
+    if not matice1 or not matice2: return None
     matice: list[list[int]] = []
     if len(matice1[0]) != len(matice2):
         return None
