0a9dd9728e76f01c1fbe7f78460c2f03135f3391
niamicz <167056957+niamicz@users.noreply.github.com>
2024-12-11 16:22:13 +0100

    Ukol8final.py
    Ukol8

--- Ukol_8/python/matice.py
+++ Ukol_8/python/matice.py
@@ -2,30 +2,78 @@ from __future__ import annotations
 from typing import Union
 import random
 
-
 class Matice:
     def __init__(self, n: int, m: int, data=None):
         """Inicializuje matici n x m."""
-        pass
+        self.n = n
+        self.m = m
+        if data is None:
+            self.data = []
+            for _ in range(n):
+                row = []
+                for _ in range(m):
+                    row.append(random.randint(0, 3))  
+                self.data.append(row)
+        else:
+            self.data = data
 
     def __str__(self) -> str:
         """Vrátí stringovou reprezentaci matice."""
-        pass
+        output = []
+        for row in self.data:
+            output.append(' '.join(map(str, row)))
+        return '\n'.join(output)
 
     def __add__(self, other: Matice) -> Matice:
         """Sečte aktuální matici s maticí other."""
-        # Implementace součtu matic
-        pass
+        if self.n != other.n or self.m != other.m:
+            raise ValueError("neplatné rozměry matic")
+        new_data = []
+        for i in range(self.n):
+            new_row = []
+            for j in range(self.m):
+                new_row.append(self.data[i][j] + other.data[i][j])
+            new_data.append(new_row)
+        return Matice(self.n, self.m, new_data)
 
     def __mul__(self, other: Union[Matice, int]) -> Union[Matice, int]:
         """Vynásobí aktuální matici maticí nebo skalárem."""
-        # Implementace násobení matic
-        pass
+        if isinstance(other, Matice):
+            if self.m != other.n:
+                raise ValueError("neplatné rozměry")
+            new_data = []
+            for i in range(self.n):
+                new_row = []
+                for j in range(other.m):
+                    suma = 0
+                    for k in range(self.m):
+                        suma += self.data[i][k] * other.data[k][j]
+                    new_row.append(suma)
+                new_data.append(new_row)
+            return Matice(self.n, other.m, new_data)
+        elif isinstance(other, int):
+            new_data = []
+            for i in range(self.n):
+                new_row = [val * other for val in self.data[i]]
+                new_data.append(new_row)
+            return Matice(self.n, self.m, new_data)
+        else:
+            raise TypeError("neplatné násobení")
 
     def transpozice(self) -> Matice:
         """Vrátí transponovanou matici."""
-        # Implementace transpozice matice
+        new_data = []
+        for i in range(self.m):
+            new_row = []
+            for j in range(self.n):
+                new_row.append(self.data[j][i])
+            new_data.append(new_row)
+        return Matice(self.m, self.n, new_data)
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Matice):
+            return False
+        return self.n == other.n and self.m == other.m and self.data == other.data
 
 if __name__ == "__main__":
     # Vytvořte instance třídy Matice a otestujte metody
