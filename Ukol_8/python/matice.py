5b48e4c40c140ad1c28583a440fc301f935a96ff
otakarszaffner <75946054+otakarszaffner@users.noreply.github.com>
2024-12-11 12:27:13 +0100

    Update matice.py
    prvni verze

--- Ukol_8/python/matice.py
+++ Ukol_8/python/matice.py
@@ -2,29 +2,74 @@ from __future__ import annotations
 from typing import Union
 import random
 
+MIN_VALUE = 0
+MAX_VALUE = 9
 
 class Matice:
     def __init__(self, n: int, m: int, data=None):
         """Inicializuje matici n x m."""
-        pass
+        self.n = n
+        self.m = m
+        if data is None:
+            self.matice = [[random.randint(MIN_VALUE, MAX_VALUE) for a in range(m)] for a in range(n)]
+        else:
+            self.matice = data
 
     def __str__(self) -> str:
         """Vrátí stringovou reprezentaci matice."""
-        pass
+        if not self.matice: return ""
+        else: return '\n'.join(' '.join(map(str, row)) for row in self.matice)
 
     def __add__(self, other: Matice) -> Matice:
         """Sečte aktuální matici s maticí other."""
         # Implementace součtu matic
-        pass
+        if not self.matice and not other: return []
+        matice: list[list[int]] = []
+        if len(self.matice) != len(other.matice) or any(len(row1) != len(row2) for row1, row2 in zip(self.matice, other.matice)):
+            return None
+    
+        matice = []
+        for r in range(len(self.matice)):
+            row = []
+            for c in range(len(self.matice[0])):
+                row.append(self.matice[r][c] + other.matice[r][c])
+            matice.append(row)
+        
+        return matice
 
     def __mul__(self, other: Union[Matice, int]) -> Union[Matice, int]:
         """Vynásobí aktuální matici maticí nebo skalárem."""
         # Implementace násobení matic
-        pass
+        if isinstance(other, int):
+            return Matice(self.n, self.m, [[other*num for num in row] for row in self.matice])
+        if not self.matice and not other.matice:
+            return []
+        if not self.matice and not other.matice:
+            return None
+        if len(self.matice[0]) != len(other.matice):
+            return None
+        matice = []
+
+        for r in range(len(self.matice)):
+            row = []
+            for c in range(len(other.matice[0])):
+                row.append(sum(self.matice[r][d] * other.matice[d][c] for d in range(len(other.matice))))
+            matice.append(row)
+        return matice
 
     def transpozice(self) -> Matice:
         """Vrátí transponovanou matici."""
         # Implementace transpozice matice
+        if not self.matice: return []
+        if not self.matice[0]: return [[]]
+
+        matice = []
+        for r in range(len(self.matice)):
+            row = []
+            for c in range(len(self.matice[0])):
+                row.append(self.matice[c][r])
+            matice.append(row)
+        return matice
 
 
 if __name__ == "__main__":
