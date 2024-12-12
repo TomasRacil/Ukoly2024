d657aa1509af068ec6e0ece2bcb90015852a151a
schizo-coder-i-came-from-universe <kaspik2003@seznam.cz>
2024-12-12 22:49:34 +0100

    6.6

--- Ukol_6/cpp/hanoi.cpp
+++ Ukol_6/cpp/hanoi.cpp
@@ -29,16 +29,17 @@ void hanoi(int n, char z, char pomocny, char cil, vector<vector<int> >& veze, ve
         for (int i=n;i>0;--i)
             veze[0].push_back(i);
     if (n == 1) {
-        Tah tah = {n, z, cil, veze};
-        tahy.push_back(tah);
+        Tah tah = {1, z, cil, veze};
         provedTah(veze, tah);
+        tahy.push_back(tah);
         tah.stavVezi = veze;
+        } 
         
-    } else {
+    else {
         hanoi(n - 1, z, cil, pomocny, veze, tahy);
         Tah tah = {n, z, cil, veze};
-        tahy.push_back(tah);
         provedTah(veze, tah);
+        tahy.push_back(tah);
         tah.stavVezi = veze;
         hanoi(n - 1, pomocny, z, cil, veze, tahy);
             }
