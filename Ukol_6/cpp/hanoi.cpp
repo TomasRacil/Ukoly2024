9dd83dcb10a812dec2ed6a5aa331cf5cff77463a
Daniel Borovička <borovickadaniel8@gmail.com>
2025-01-27 09:42:16 +0100

    Všechny Ukoly - Borovička Daniel (#49)
    * Borovička Final sifry.cpp
    
    * Update CMakeLists.txt
    
    * Update vypocty.py
    
    * Update CMakeLists.txt
    
    * Update vypocty.py
    
    * Update vypocty.py
    
    * Update vypocty.cpp
    
    * Update linked_list.cpp
    
    * Update linked_list.cpp
    
    * opravy testů
    
    * TEST sync
    
    * Update sifry.cpp
    
    * Update vypocty.py
    
    * Update vypocty.py
    
    * Ukol 4 přidán
    
    * Ukol 4 fix
    
    * Ukol 5 pridan
    
    * Update testCpp.yml
    
    * Úkol 6 pridan
    
    * Update hello_world.cpp
    
    * Update hello_world.py
    
    * Update vypocty.cpp
    
    * Borovička hanoi.cpp
    
    * Borovička ošetřené - hanoi.cpp
    
    * Ukol 7 pridan
    
    * Borovička ošetřeno - hanoi.cpp
    
    * Update hello_world.cpp
    
    * Update hello_world.py
    
    * Update hello_world.cpp
    
    * Update hello_world.py
    
    * Update vypocty.py
    
    * Update sifry.cpp
    
    * Update linked_list.cpp
    
    * Update sifry.cpp
    
    * Update sifry.cpp
    
    * Ukol 8 pridan opraven test ukolu 7
    
    * Update matice.py
    
    * Ukol 9 pridan
    
    * Update matice.py
    
    * Update matice.py
    
    * Update matice.py
    
    * Update matice.py
    
    * Update matice.py
    
    * Update advent_lod.cpp
    
    * Ukol 9 workflow
    
    * Ukol 9 aktualizace zadani
    
    * Update main.py
    
    * Update main.py
    
    * Update main.py
    
    * Update main.py
    
    * Update Knihovna.py
    
    * Update Kniha.py
    
    * Update Ctenar.py
    
    * Update main.py
    
    ---------
    
    Co-authored-by: tomasracil <tomas.racil@unob.cz>
    Co-authored-by: TomasRacil <72918819+TomasRacil@users.noreply.github.com>

---
+++ Ukol_6/cpp/hanoi.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Struktura pro reprezentaci tahu
+struct Tah
+{
+    int disk;
+    char z;
+    char na;
+
+    vector<vector<int>> stavVezi; // Člen pro uložení stavu věží po provedení tahu
+};
+
+// Funkce pro provedení tahu
+void provedTah(vector<vector<int>>& veze, Tah tah)
+{
+	int disk = veze[tah.z - 'A'].back(); // Vyjme disk z počátečního kolíku
+	veze[tah.z - 'A'].pop_back(); 
+	veze[tah.na - 'A'].push_back(disk); // Vloží disk na cílový kolík
+}
+
+// Funkce pro řešení Hanoiských věží (bez výpisu)
+void hanoi(int n, char z, char pomocny, char cil, vector<vector<int>>& veze, vector<Tah>& tahy)
+{
+    if (n <= 0) return;
+	if (veze[0].empty() && veze[1].empty() && veze[2].empty()) // Pokud jsou první dva kolíky prázdné, přesuneme disk na první kolík
+	{
+		for (int i = n; i > 0; i--)
+		{
+			veze[0].push_back(i);
+		}
+	}
+    if (n == 1)
+    {
+		Tah tah = { n, z, cil, veze }; // Vytvoření tahu
+		provedTah(veze, tah); 
+		tah.stavVezi = veze; // Uložení stavu věží po provedení tahu
+		tahy.push_back(tah); // Uložení tahu do vektoru tahů
+    }
+    else
+    {
+		hanoi(n - 1, z, cil, pomocny, veze, tahy); // Rekurzivní volání pro přesunutí n-1 disků na pomocný kolík
+		Tah tah = { n, z, cil, veze }; 
+        provedTah(veze, tah);
+		tah.stavVezi = veze;   // Uložení stavu věží po provedení tahu
+        tahy.push_back(tah);
+        hanoi(n - 1, pomocny, z, cil, veze, tahy);
+    }
+}
+
+void zobrazVeze(vector<vector<int>>& veze)
+{
+	for (int i = 0; i < 3; i++) // Pro každý kolík vypíše jeho obsah 
+    {
+		cout << "Kolík " << char('A' + i) << ": "; // Výpis kolíku
+		for (int disk : veze[i]) // Výpis disků na kolíku
+        {
+            cout << disk << " ";
+        }
+        cout << endl;
+    }
+}
+
+#ifndef __TEST__
+int main()
+{
+    int n;
+    do
+    {
+        cout << "Zadejte pocet disku (kladne cislo): ";
+        cin >> n;
+		cin.ignore(); // Ignorování zbytku řádku
+		if (n <= 0) // Kontrola zda je zadané číslo kladné
+        {
+            cout << "Pocet disku musi byt kladne cislo. Zkuste to znovu." << endl;
+        }
+	} while (n <= 0); // Cyklus se opakuje dokud není zadané kladné číslo
+
+    vector<vector<int>> veze(3);
+    for (int i = n; i > 0; i--)
+    {
+        veze[0].push_back(i);
+    }
+
+    vector<Tah> tahy; // Vektor pro uložení tahů
+    hanoi(n, 'A', 'B', 'C', veze, tahy);
+
+    // Zobrazení tahů a stavů věží
+    for (Tah tah : tahy)
+    {
+        cout << "Přesuň disk " << tah.disk << " z kolíku " << tah.z << " na kolík " << tah.na << endl;
+        zobrazVeze(tah.stavVezi); // Zobrazení stavu věží po tahu
+    }
+
+    return 0;
+}
+#endif // __TEST__
